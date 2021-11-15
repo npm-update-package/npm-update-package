6f174e3c02e5afe4fe35e023ce673589f5fc8e1e
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-16 03:22:57 +0900

    add PackageVersion.ts

---
+++ src/PackageVersion.ts
@@ -0,0 +1,44 @@
+import { coerce } from 'semver'
+
+export class PackageVersion {
+  readonly version: string
+  readonly major: number
+  readonly minor: number
+  readonly patch: number
+
+  constructor ({
+    version,
+    major,
+    minor,
+    patch
+  }: {
+    version: string
+    major: number
+    minor: number
+    patch: number
+  }) {
+    this.version = version
+    this.major = major
+    this.minor = minor
+    this.patch = patch
+  }
+
+  static of (version: string): PackageVersion {
+    const semver = coerce(version)
+
+    if (semver === null) {
+      throw new Error(`Failed to parse package version. version=${version}`)
+    }
+
+    return new PackageVersion({
+      version: semver.version,
+      major: semver.major,
+      minor: semver.minor,
+      patch: semver.patch
+    })
+  }
+
+  toString (): string {
+    return this.version
+  }
+}
