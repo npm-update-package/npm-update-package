177b841a48f8bd4e2786c85be9272fea13c90a9e
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-16 03:53:08 +0900

    add toOutdatedPackages.ts

---
+++ src/toOutdatedPackages.ts
@@ -0,0 +1,14 @@
+import type { NcuOutdatedPackages } from './types/NcuOutdatedPackages'
+import type { OutdatedPackage } from './types/OutdatedPackage'
+import { PackageVersion } from './PackageVersion'
+import { readAllDependencies } from './readAllDependencies'
+
+export const toOutdatedPackages = async (outdatedPackages: NcuOutdatedPackages): Promise<OutdatedPackage[]> => {
+  const dependencies = await readAllDependencies('./package.json')
+  return Object.entries(outdatedPackages)
+    .map(([name, newVersion]) => ({
+      name,
+      currentVersion: PackageVersion.of(dependencies[name]),
+      newVersion: PackageVersion.of(newVersion)
+    }))
+}
