3dc706f6e1508d460e742c76a509803d6dab4a3d
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-16 04:25:19 +0900

    add Terminal.ts

---
+++ src/Terminal.ts
@@ -0,0 +1,29 @@
+import execa from 'execa'
+import type { ExecaReturnValue } from 'execa'
+import { isExecaReturnValue } from './isExecaReturnValue'
+
+export class Terminal {
+  async run (
+    command: string,
+    ...args: string[]
+  ): Promise<ExecaReturnValue<string>> {
+    return await execa(command, args)
+  }
+
+  async runWithErrorHandling (
+    command: string,
+    ...args: string[]
+  ): Promise<ExecaReturnValue<string>> {
+    try {
+      return await this.run(command, ...args)
+    } catch (e) {
+      const value: unknown = e instanceof Error ? JSON.parse(JSON.stringify(e)) : e
+
+      if (isExecaReturnValue(value)) {
+        return value
+      } else {
+        throw e
+      }
+    }
+  }
+}
