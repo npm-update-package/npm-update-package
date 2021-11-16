eaf661a019eeac8b78c352072ba2088aa6a6960b
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-16 12:41:04 +0900

    refactor createBranchName

--- src/createBranchName.ts
+++ src/createBranchName.ts
@@ -2,6 +2,6 @@ import type { OutdatedPackage } from './types/OutdatedPackage'
 
 export const createBranchName = (outdatedPackage: OutdatedPackage): string => {
   const packageName = outdatedPackage.name
-  const newVersion = outdatedPackage.newVersion.toString()
+  const newVersion = outdatedPackage.newVersion.version
   return `npm-update-package/${packageName}/v${newVersion}`
 }
