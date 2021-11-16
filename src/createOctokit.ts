b3fa9a74edb9f3f75ed53d7d4d0adcc2bf5dd461
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-16 12:37:36 +0900

    add createOctokit.ts

---
+++ src/createOctokit.ts
@@ -0,0 +1,25 @@
+import { Octokit } from '@octokit/rest'
+import pkg from '../package.json'
+import type { GitRepository } from './GitRepository'
+
+export const createOctokit = (repository: GitRepository): Octokit => {
+  if (process.env.GITHUB_TOKEN === undefined) {
+    throw new Error('process.env.GITHUB_TOKEN is undefined')
+  }
+
+  const auth = `token ${process.env.GITHUB_TOKEN}`
+  const userAgent = `${pkg.name}/${pkg.version}`
+
+  if (repository.isGitHubDotCom) {
+    return new Octokit({
+      auth,
+      userAgent
+    })
+  } else {
+    return new Octokit({
+      auth,
+      userAgent,
+      baseUrl: repository.apiEndPoint
+    })
+  }
+}
