e1e3924e5dcc88f538978a8233ab0aedbfec0faf
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-16 20:40:59 +0900

    add createCommitMessage.ts

---
+++ src/createCommitMessage.test.ts
@@ -0,0 +1,13 @@
+import { createCommitMessage } from './createCommitMessage'
+import { PackageVersion } from './PackageVersion'
+
+describe('createCommitMessage', () => {
+  it('returns commit message', () => {
+    const actual = createCommitMessage({
+      name: 'package-name',
+      currentVersion: PackageVersion.of('1.0.0'),
+      newVersion: PackageVersion.of('1.2.3')
+    })
+    expect(actual).toBe('chore(deps): update dependency package-name to v1.2.3')
+  })
+})
---
+++ src/createCommitMessage.ts
@@ -0,0 +1,7 @@
+import type { OutdatedPackage } from './types/OutdatedPackage'
+
+export const createCommitMessage = (outdatedPackage: OutdatedPackage): string => {
+  const packageName = outdatedPackage.name
+  const newVersion = outdatedPackage.newVersion.version
+  return `chore(deps): update dependency ${packageName} to v${newVersion}`
+}
