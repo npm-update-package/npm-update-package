e3933b65dd32d5e121047f6d9df07baa07df8bc7
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-16 12:44:07 +0900

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,38 @@
+import { createBranchName } from './createBranchName'
+import { createGitHub } from './createGitHub'
 import { getOutdatedPackages } from './getOutdatedPackages'
+import { Git } from './Git'
+import { Terminal } from './Terminal'
 
 export const main = async (): Promise<void> => {
-  // get outdated packages
   const outdatedPackages = await getOutdatedPackages()
   console.debug({ outdatedPackages })
+
+  const terminal = new Terminal()
+  const git = new Git(terminal)
+  const gitRepo = await git.getRepository()
+  const github = createGitHub(gitRepo)
+  const remoteBranches = await github.fetchBranches({
+    owner: gitRepo.owner,
+    repo: gitRepo.name
+  })
+  console.debug({ remoteBranches })
+
+  const remoteBranchNames = remoteBranches.map(({ name }) => name)
+  console.debug({ remoteBranchNames })
 
   for (const outdatedPackage of outdatedPackages) {
     console.debug({ outdatedPackage })
 
-    // TODO: create branch
+    const branchName = createBranchName(outdatedPackage)
+    console.debug({ branchName })
+
+    if (remoteBranchNames.includes(branchName)) {
+      console.log(`${branchName} branch already exists on ${gitRepo.url}`)
+      continue
+    }
+
+    await git.createBranch(branchName)
 
     // TODO: update packages
 
