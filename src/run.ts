7d1dfc6f8e7162c82749a49afaadb6e65ea62eea
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-17 02:12:30 +0900

    update run.ts

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,3 @@
 import { main } from './main'
 
-console.log('start')
-main()
-  .then(() => {
-    console.log('end')
-  })
-  .catch(e => {
-    console.error(e)
-  })
+main().catch(e => { throw e })
