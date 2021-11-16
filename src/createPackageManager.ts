358dbd285dbabf01de97d35a0e9770c8f9c7e4ca
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-16 20:32:08 +0900

    add createPackageManager.ts

---
+++ src/createPackageManager.ts
@@ -0,0 +1,15 @@
+import type { Terminal } from './Terminal'
+import type { PackageManager } from './package-manager/PackageManager'
+import { Npm } from './package-manager/Npm'
+import { Yarn } from './package-manager/Yarn'
+
+export const createPackageManager = (terminal: Terminal): PackageManager => {
+  switch (process.env.PACKAGE_MANAGER) {
+    case 'npm':
+      return new Npm(terminal)
+    case 'yarn':
+      return new Yarn(terminal)
+    default:
+      throw new Error(`process.env.PACKAGE_MANAGER is invalid. process.env.PACKAGE_MANAGER=${process.env.PACKAGE_MANAGER ?? 'undefined'}`)
+  }
+}
