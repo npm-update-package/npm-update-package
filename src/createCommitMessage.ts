0bd4b89fbe02bb373a99a431eb2e177cd0e88053
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-18 01:47:59 +0900

    refactor createCommitMessage

--- src/createCommitMessage.ts
+++ src/createCommitMessage.ts
@@ -1,10 +1,7 @@
 import type { OutdatedPackage } from './types/OutdatedPackage'
 
-// TODO: add logs using logger
 // TODO: include update type (major/minor/patch)
 // TODO: include current version
 export const createCommitMessage = (outdatedPackage: OutdatedPackage): string => {
-  const packageName = outdatedPackage.name
-  const newVersion = outdatedPackage.newVersion.version
-  return `chore(deps): update dependency ${packageName} to v${newVersion}`
+  return `chore(deps): update dependency ${outdatedPackage.name} to v${outdatedPackage.newVersion.version}`
 }
