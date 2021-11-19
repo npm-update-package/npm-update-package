53cb7a49fabe571b474df0ad0dc936bf4b55338a
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-20 02:37:35 +0900

    update GitHub

--- src/GitHub.ts
+++ src/GitHub.ts
@@ -5,9 +5,9 @@ import type {
 import type { ValuesType } from 'utility-types'
 
 export type Branch = ValuesType<RestEndpointMethodTypes['repos']['listBranches']['response']['data']>
+export type PullRequest = RestEndpointMethodTypes['pulls']['create']['response']['data']
 export type Repository = RestEndpointMethodTypes['repos']['get']['response']['data']
 
-// TODO: add logs using logger
 export class GitHub {
   constructor (private readonly octokit: Octokit) {}
 
@@ -21,7 +21,7 @@ export class GitHub {
     return data
   }
 
-  async createPullRequest (params: RestEndpointMethodTypes['pulls']['create']['parameters']): Promise<RestEndpointMethodTypes['pulls']['create']['response']['data']> {
+  async createPullRequest (params: RestEndpointMethodTypes['pulls']['create']['parameters']): Promise<PullRequest> {
     const { data } = await this.octokit.pulls.create(params)
     return data
   }
