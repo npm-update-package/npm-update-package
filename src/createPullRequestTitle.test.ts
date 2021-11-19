d4976f8bc5f22f97e384340ade411279d7b14a00
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-20 02:30:10 +0900

    update createPullRequestTitle

--- src/createPullRequestTitle.test.ts
+++ src/createPullRequestTitle.test.ts
@@ -8,6 +8,6 @@ describe('createPullRequestTitle', () => {
       currentVersion: PackageVersion.of('1.0.0'),
       newVersion: PackageVersion.of('1.2.3')
     })
-    expect(actual).toBe('chore(deps): update dependency package-name to v1.2.3')
+    expect(actual).toBe('chore(deps): update package-name to v1.2.3')
   })
 })
--- src/createPullRequestTitle.ts
+++ src/createPullRequestTitle.ts
@@ -1,7 +1,8 @@
 import type { OutdatedPackage } from './types/OutdatedPackage'
 
 // TODO: include update type (major/minor/patch)
-// TODO: include current version
 export const createPullRequestTitle = (outdatedPackage: OutdatedPackage): string => {
-  return `chore(deps): update dependency ${outdatedPackage.name} to v${outdatedPackage.newVersion.version}`
+  const packageName = outdatedPackage.name
+  const newVersion = outdatedPackage.newVersion.version
+  return `chore(deps): update ${packageName} to v${newVersion}`
 }
