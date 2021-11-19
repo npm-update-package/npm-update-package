42b0693a35a0aaac57c85085706bdd8053fb4233
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-19 23:16:36 +0900

    add PullRequestCreator.ts

---
+++ src/PullRequestCreator.ts
@@ -0,0 +1,54 @@
+import type { OutdatedPackage } from './types/OutdatedPackage'
+import type { GitRepository } from './values/GitRepository'
+import { createPullRequestBody } from './createPullRequestBody'
+import { createPullRequestTitle } from './createPullRequestTitle'
+import type {
+  GitHub,
+  Repository as GitHubRepository
+} from './GitHub'
+import { logger } from './logger'
+
+export class PullRequestCreator {
+  private readonly github: GitHub
+  private readonly gitRepo: GitRepository
+  private readonly githubRepo: GitHubRepository
+
+  constructor ({
+    github,
+    gitRepo,
+    githubRepo
+  }: {
+    github: GitHub
+    gitRepo: GitRepository
+    githubRepo: GitHubRepository
+  }) {
+    this.github = github
+    this.gitRepo = gitRepo
+    this.githubRepo = githubRepo
+  }
+
+  async create ({
+    outdatedPackage,
+    branchName
+  }: {
+    outdatedPackage: OutdatedPackage
+    branchName: string
+  }): Promise<void> {
+    const title = createPullRequestTitle(outdatedPackage)
+    logger.debug(`title=${title}`)
+
+    const body = createPullRequestBody(outdatedPackage)
+    logger.debug(`body=${body}`)
+
+    const createdPullRequest = await this.github.createPullRequest({
+      owner: this.gitRepo.owner,
+      repo: this.gitRepo.name,
+      base: this.githubRepo.default_branch,
+      head: branchName,
+      title,
+      body
+    })
+    logger.debug(`createdPullRequest=${JSON.stringify(createdPullRequest)}`)
+    logger.info(`Pull request for ${outdatedPackage.name} has created. ${createdPullRequest.html_url}`)
+  }
+}
