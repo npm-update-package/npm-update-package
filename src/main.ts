5bdcc824cbbdb51be2a33501100993c62e8598c6
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-19 22:25:28 +0900

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
-// TODO: Keep it less than 100 lines.
-
-import { createBranchName } from './createBranchName'
-import { createCommitMessage } from './createCommitMessage'
 import { createGitHub } from './createGitHub'
 import { createPackageManager } from './createPackageManager'
-import { createPullRequestBody } from './createPullRequestBody'
-import { createPullRequestTitle } from './createPullRequestTitle'
 import { getOutdatedPackages } from './getOutdatedPackages'
 import { logger } from './logger'
-import { updateOutdatedPackage } from './updateOutdatedPackage'
 import { Git } from './Git'
 import { Terminal } from './Terminal'
+import { OutdatedPackageUpdater } from './OutdatedPackageUpdater'
+import { RemoteBranchExistenceChecker } from './RemoteBranchExistenceChecker'
 
 export const main = async (): Promise<void> => {
-  // Get outdated packages
   const outdatedPackages = await getOutdatedPackages()
   logger.debug(`outdatedPackages=${JSON.stringify(outdatedPackages)}`)
 
@@ -24,13 +18,11 @@ export const main = async (): Promise<void> => {
 
   logger.info(`There are ${outdatedPackages.length} outdated packages.`)
 
-  // Get Git repository
   const terminal = new Terminal()
   const git = new Git(terminal)
   const gitRepo = await git.getRepository()
   logger.debug(`gitRepo=${JSON.stringify(gitRepo)}`)
 
-  // Get GitHub repository
   const github = createGitHub(gitRepo)
   const githubRepo = await github.fetchRepository({
     owner: gitRepo.owner,
@@ -38,85 +30,25 @@ export const main = async (): Promise<void> => {
   })
   logger.debug(`githubRepo=${JSON.stringify(githubRepo)}`)
 
-  // Get remote branches
   const remoteBranches = await github.fetchBranches({
     owner: gitRepo.owner,
     repo: gitRepo.name
   })
   logger.debug(`remoteBranches=${JSON.stringify(remoteBranches)}`)
-  const remoteBranchNames = remoteBranches.map(({ name }) => name)
-  logger.debug(`remoteBranchNames=${JSON.stringify(remoteBranchNames)}`)
-  const packageManager = createPackageManager(terminal)
 
-  // Update outdated packages
+  const remoteBranchExistenceChecker = RemoteBranchExistenceChecker.of(remoteBranches)
+  const packageManager = createPackageManager(terminal)
+  const outdatedPackageUpdater = new OutdatedPackageUpdater({
+    git,
+    gitRepo,
+    github,
+    githubRepo,
+    packageManager,
+    remoteBranchExistenceChecker
+  })
+
   for (const outdatedPackage of outdatedPackages) {
     logger.debug(`outdatedPackage=${JSON.stringify(outdatedPackage)}`)
-
-    // Create branch
-    const branchName = createBranchName(outdatedPackage)
-    logger.debug(`branchName=${branchName}`)
-
-    if (remoteBranchNames.includes(branchName)) {
-      logger.info(`Skip ${outdatedPackage.name} because ${branchName} branch already exists on remote.`)
-      continue
-    }
-
-    await git.createBranch(branchName)
-    logger.info(`${branchName} branch has created.`)
-
-    // Update outdated package
-    const updatedPackages = await updateOutdatedPackage(outdatedPackage)
-    logger.debug(`updatedPackages=${JSON.stringify(updatedPackages)}`)
-
-    if (updatedPackages.length !== 1) {
-      throw new Error(`Failed to update ${outdatedPackage.name}.`)
-    }
-
-    await packageManager.install()
-    logger.info(`${outdatedPackage.name} has updated from v${outdatedPackage.currentVersion.version} to v${outdatedPackage.newVersion.version}`)
-
-    // Commit files
-    // TODO: add only necessary files （package.json & package-lock.json or yarn.lock）
-    await git.addAll()
-    const message = createCommitMessage(outdatedPackage)
-    logger.debug(`message=${message}`)
-
-    if (process.env.GIT_USER_NAME !== undefined && process.env.GIT_USER_EMAIL !== undefined) {
-      const name = await git.getConfig('user.name')
-      logger.debug(`name=${name}`)
-      const email = await git.getConfig('user.email')
-      logger.debug(`email=${email}`)
-      await git.setConfig('user.name', process.env.GIT_USER_NAME)
-      await git.setConfig('user.email', process.env.GIT_USER_EMAIL)
-      await git.commit(message)
-      await git.setConfig('user.name', name)
-      await git.setConfig('user.email', email)
-    } else {
-      await git.commit(message)
-    }
-
-    // Push branch to remote
-    await git.push(branchName)
-
-    // Create pull request
-    const title = createPullRequestTitle(outdatedPackage)
-    logger.debug(`title=${title}`)
-    const body = createPullRequestBody(outdatedPackage)
-    logger.debug(`body=${body}`)
-    const pullRequest = await github.createPullRequest({
-      owner: gitRepo.owner,
-      repo: gitRepo.name,
-      base: githubRepo.default_branch,
-      head: branchName,
-      title,
-      body
-    })
-    logger.debug(`pullRequest=${JSON.stringify(pullRequest)}`)
-    logger.info(`Pull request for ${outdatedPackage.name} has created. ${pullRequest.html_url}`)
-
-    // Remove branch
-    await git.checkout('-')
-    await git.removeBranch(branchName)
-    logger.info(`${branchName} branch has removed.`)
+    await outdatedPackageUpdater.update(outdatedPackage)
   }
 }
