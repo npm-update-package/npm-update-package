08fded79306226642b98b7f61bf6742b0b67306b
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-20 13:21:38 +0900

    update updateOutdatedPackage

--- src/updateOutdatedPackage.ts
+++ src/updateOutdatedPackage.ts
@@ -1,22 +1,12 @@
-import { run } from 'npm-check-updates'
 import type { OutdatedPackage } from './types/OutdatedPackage'
-import { isNcuOutdatedPackages } from './isNcuOutdatedPackages'
-import { logger } from './logger'
-import { toOutdatedPackages } from './toOutdatedPackages'
+import { runNcu } from './runNcu'
 
 // TODO: add test
 // TODO: rename to updatePackageJson (or execute PackageManager.install() here)
 export const updateOutdatedPackage = async (outdatedPackage: OutdatedPackage): Promise<OutdatedPackage[]> => {
-  const result = await run({
+  return await runNcu({
     jsonUpgraded: true,
     filter: outdatedPackage.name,
     upgrade: true
   })
-  logger.debug(`result=${JSON.stringify(result)}`)
-
-  if (!isNcuOutdatedPackages(result)) {
-    throw new Error('result is not NcuOutdatedPackages')
-  }
-
-  return await toOutdatedPackages(result)
 }
