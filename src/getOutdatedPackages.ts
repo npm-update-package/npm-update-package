8865c64ac5c2b2ee1ad78a3140a4bffc2f6a73e1
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-20 23:46:27 +0900

    remove getOutdatedPackages.ts

--- src/getOutdatedPackages.ts
+++
@@ -1,9 +0,0 @@
-import type { OutdatedPackage } from './types'
-import { runNcu } from './runNcu'
-
-// TODO: add test
-export const getOutdatedPackages = async (): Promise<OutdatedPackage[]> => {
-  return await runNcu({
-    jsonUpgraded: true
-  })
-}
