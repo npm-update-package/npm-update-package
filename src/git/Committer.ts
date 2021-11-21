5e98e9ad69b79b96dafb3ed39b91f900d294eb05
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-21 13:40:41 +0900

    create Committer

---
+++ src/git/Committer.ts
@@ -0,0 +1,20 @@
+import type { Git } from './Git'
+
+// TODO: add test
+export class Committer {
+  constructor (private readonly git: Git) {}
+
+  async commit (message: string): Promise<void> {
+    if (process.env.GIT_USER_NAME !== undefined && process.env.GIT_USER_EMAIL !== undefined) {
+      const name = await this.git.getConfig('user.name')
+      const email = await this.git.getConfig('user.email')
+      await this.git.setConfig('user.name', process.env.GIT_USER_NAME)
+      await this.git.setConfig('user.email', process.env.GIT_USER_EMAIL)
+      await this.git.commit(message)
+      await this.git.setConfig('user.name', name)
+      await this.git.setConfig('user.email', email)
+    } else {
+      await this.git.commit(message)
+    }
+  }
+}
--- src/git/index.ts
+++ src/git/index.ts
@@ -1,3 +1,4 @@
+export { Committer } from './Committer'
 export { Git } from './Git'
 export type { GitRepository } from './GitRepository'
 export { BranchCleaner } from './BranchCleaner'
