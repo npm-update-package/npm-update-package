257141ed1c7bf684f8833fa65cb2c25bf0a3f369
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-21 16:13:58 +0900

    update tests

--- src/github/createPullRequestTitle.test.ts
+++ src/github/createPullRequestTitle.test.ts
@@ -8,36 +8,36 @@ describe('createPullRequestTitle', () => {
   describe('if update type is patch', () => {
     it('returns pull request title', () => {
       const actual = createPullRequestTitle({
-        name: 'package-name',
+        name: '@typescript-eslint/eslint-plugin',
         currentVersion,
         newVersion: PackageVersion.of('1.0.1'),
         type: UpdateType.Patch
       })
-      expect(actual).toBe('chore(deps): patch update package-name to v1.0.1')
+      expect(actual).toBe('chore(deps): patch update @typescript-eslint/eslint-plugin to v1.0.1')
     })
   })
 
   describe('if update type is minor', () => {
     it('returns pull request title', () => {
       const actual = createPullRequestTitle({
-        name: 'package-name',
+        name: '@typescript-eslint/eslint-plugin',
         currentVersion,
         newVersion: PackageVersion.of('1.1.0'),
         type: UpdateType.Minor
       })
-      expect(actual).toBe('chore(deps): minor update package-name to v1.1.0')
+      expect(actual).toBe('chore(deps): minor update @typescript-eslint/eslint-plugin to v1.1.0')
     })
   })
 
   describe('if update type is major', () => {
     it('returns pull request title', () => {
       const actual = createPullRequestTitle({
-        name: 'package-name',
+        name: '@typescript-eslint/eslint-plugin',
         currentVersion,
         newVersion: PackageVersion.of('2.0.0'),
         type: UpdateType.Major
       })
-      expect(actual).toBe('chore(deps): major update package-name to v2.0.0')
+      expect(actual).toBe('chore(deps): major update @typescript-eslint/eslint-plugin to v2.0.0')
     })
   })
 })
--- src/outdated-package-processor/createBranchName.test.ts
+++ src/outdated-package-processor/createBranchName.test.ts
@@ -4,11 +4,11 @@ import { createBranchName } from './createBranchName'
 describe('createBranchName', () => {
   it('returns branch name', () => {
     const actual = createBranchName({
-      name: 'package-name',
+      name: '@typescript-eslint/eslint-plugin',
       currentVersion: PackageVersion.of('1.0.0'),
       newVersion: PackageVersion.of('1.2.3'),
       type: 'major'
     })
-    expect(actual).toBe('npm-update-package/package-name/v1.2.3')
+    expect(actual).toBe('npm-update-package/@typescript-eslint/eslint-plugin/v1.2.3')
   })
 })
