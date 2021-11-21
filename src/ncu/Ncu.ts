28d0935a517ec322ae851ad431ec990f2e61fe7e
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-21 15:46:45 +0900

    update Ncu

--- src/ncu/Ncu.ts
+++ src/ncu/Ncu.ts
@@ -1,9 +1,10 @@
 import { run } from 'npm-check-updates'
 import type { Options } from 'npm-check-updates/build/src/types'
 import { logger } from '../logger'
+import { readAllDependencies } from '../read-all-dependencies'
 import type { OutdatedPackage } from '../types'
 import { isNcuOutdatedPackages } from './isNcuOutdatedPackages'
-import { toOutdatedPackages } from './toOutdatedPackages'
+import { NcuOutdatedPackagesConverter } from './NcuOutdatedPackagesConverter'
 
 // TODO: add test
 export class Ncu {
@@ -22,6 +23,7 @@ export class Ncu {
   }
 
   private async run (options: Options): Promise<OutdatedPackage[]> {
+    const currentDependencies = await readAllDependencies('./package.json')
     const result = await run(options)
     logger.debug(`result=${JSON.stringify(result)}`)
 
@@ -29,6 +31,7 @@ export class Ncu {
       throw new Error('result is not NcuOutdatedPackages')
     }
 
-    return await toOutdatedPackages(result)
+    const ncuOutdatedPackagesConverter = new NcuOutdatedPackagesConverter(currentDependencies)
+    return ncuOutdatedPackagesConverter.toOutdatedPackages(result)
   }
 }
