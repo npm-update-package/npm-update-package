e82dde9261ac67b940cd481e7e1e8947c9edf8e7
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-21 10:47:39 +0900

    update OutdatedPackagesUpdater

--- src/outdated-packages-updater/OutdatedPackagesUpdater.ts
+++ src/outdated-packages-updater/OutdatedPackagesUpdater.ts
@@ -1,15 +1,23 @@
 import { logger } from '../logger'
-import type { OutdatedPackageUpdater } from '../outdated-package-updater'
+import type {
+  OutdatedPackageUpdater,
+  UpdateResult
+} from '../outdated-package-updater'
 import type { OutdatedPackage } from '../types'
 
 export class OutdatedPackagesUpdater {
   constructor (private readonly outdatedPackageUpdater: OutdatedPackageUpdater) {}
 
-  async update (outdatedPackages: OutdatedPackage[]): Promise<void> {
-    // NOTE: DO NOT run in parallel
+  // NOTE: DO NOT run OutdatedPackageUpdater.update() in parallel
+  async update (outdatedPackages: OutdatedPackage[]): Promise<UpdateResult[]> {
+    const results: UpdateResult[] = []
+
     for (const outdatedPackage of outdatedPackages) {
       logger.debug(`outdatedPackage=${JSON.stringify(outdatedPackage)}`)
-      await this.outdatedPackageUpdater.update(outdatedPackage)
+      const result = await this.outdatedPackageUpdater.update(outdatedPackage)
+      results.push(result)
     }
+
+    return results
   }
 }
