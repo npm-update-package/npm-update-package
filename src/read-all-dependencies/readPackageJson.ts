11ecebbcdeebf190d0e8a4a5cff2119244854dd0
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-21 22:28:46 +0900

    update readPackageJson

--- src/read-all-dependencies/readPackageJson.ts
+++ src/read-all-dependencies/readPackageJson.ts
@@ -1,18 +1,9 @@
 import type { Package } from './Package'
-
-// TODO: create type definition
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const readJson = require('read-package-json')
+import { parsePackageJson } from './parsePackageJson'
+import { readFile } from './readFile'
 
 // TODO: add test
 export const readPackageJson = async (filePath: string): Promise<Package> => {
-  return await new Promise((resolve, reject) => {
-    readJson(filePath, (error: Error | null, data: Package) => {
-      if (error != null) {
-        reject(error)
-      } else {
-        resolve(data)
-      }
-    })
-  })
+  const json = await readFile(filePath)
+  return parsePackageJson(json)
 }
