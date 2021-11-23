25e6098ec1f06cbd49cfccc1c7b986e309c0f89e
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-24 01:00:11 +0900

    remove createCommitMessage

--- src/outdated-package-processor/createCommitMessage.ts
+++
@@ -1,8 +0,0 @@
-import type { OutdatedPackage } from '../types'
-
-export const createCommitMessage = (outdatedPackage: OutdatedPackage): string => {
-  const packageName = outdatedPackage.name
-  const newVersion = outdatedPackage.newVersion.version
-  const updateType = outdatedPackage.type
-  return `chore(deps): ${updateType} update ${packageName} to v${newVersion}`
-}
