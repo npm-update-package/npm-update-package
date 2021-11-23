575e1903fa490fe4697b00dfa7542c6af5373928
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-23 23:09:36 +0900

    update logs

--- src/bin.ts
+++ src/bin.ts
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
+import { app } from './app'
 import { createLogger } from './logger'
 import { main } from './main'
 import { initOptions } from './options'
 
 const options = initOptions()
 const logger = createLogger(options.logLevel)
-logger.info('Start npm-update-package.')
+logger.info(`Start ${app.name} v${app.version}`)
 
 main({
   options,
   logger
 })
-  .then(() => logger.info('End npm-update-package'))
+  .then(() => logger.info(`End ${app.name} v${app.version}`))
   .catch((e: unknown) => {
     // TODO: improve error handling
     logger.fatal('Unexpected error has occurred.', e)
