9dfef78459472d0a023d737f9f312b95f320a96f
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-23 22:08:43 +0900

    Transfer repository from @munierujp to @npm-update-package (#12)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 export const app = {
   name: 'npm-update-package',
   version: '0.1.0',
-  web: 'https://github.com/munierujp/npm-update-package'
+  web: 'https://github.com/npm-update-package/npm-update-package'
 } as const
--- src/git/GitRepository.test.ts
+++ src/git/GitRepository.test.ts
@@ -4,12 +4,12 @@ describe('GitRepository', () => {
   describe('of', () => {
     describe('if URL is HTTPS', () => {
       describe('if URL is github.com', () => {
-        const url = 'https://github.com/munierujp/npm-update-package.git'
+        const url = 'https://github.com/npm-update-package/npm-update-package.git'
 
         it('returns new GitRepository instance', () => {
           const repo = GitRepository.of(url)
           expect(repo.host).toBe('github.com')
-          expect(repo.owner).toBe('munierujp')
+          expect(repo.owner).toBe('npm-update-package')
           expect(repo.name).toBe('npm-update-package')
           expect(repo.apiEndPoint).toBe('https://api.github.com')
           expect(repo.isGitHubDotCom).toBe(true)
@@ -17,12 +17,12 @@ describe('GitRepository', () => {
       })
 
       describe('if URL is not github.com', () => {
-        const url = 'https://git.example.com/munierujp/npm-update-package.git'
+        const url = 'https://git.example.com/npm-update-package/npm-update-package.git'
 
         it('returns new GitRepository instance', () => {
           const repo = GitRepository.of(url)
           expect(repo.host).toBe('git.example.com')
-          expect(repo.owner).toBe('munierujp')
+          expect(repo.owner).toBe('npm-update-package')
           expect(repo.name).toBe('npm-update-package')
           expect(repo.apiEndPoint).toBe('https://git.example.com/api/v3')
           expect(repo.isGitHubDotCom).toBe(false)
@@ -32,12 +32,12 @@ describe('GitRepository', () => {
 
     describe('if URL is SSH', () => {
       describe('if URL is github.com', () => {
-        const url = 'git@github.com:munierujp/npm-update-package.git'
+        const url = 'git@github.com:npm-update-package/npm-update-package.git'
 
         it('returns new GitRepository instance', () => {
           const repo = GitRepository.of(url)
           expect(repo.host).toBe('github.com')
-          expect(repo.owner).toBe('munierujp')
+          expect(repo.owner).toBe('npm-update-package')
           expect(repo.name).toBe('npm-update-package')
           expect(repo.apiEndPoint).toBe('https://api.github.com')
           expect(repo.isGitHubDotCom).toBe(true)
@@ -45,12 +45,12 @@ describe('GitRepository', () => {
       })
 
       describe('if URL is not github.com', () => {
-        const url = 'git@git.example.com:munierujp/npm-update-package.git'
+        const url = 'git@git.example.com:npm-update-package/npm-update-package.git'
 
         it('returns new GitRepository instance', () => {
           const repo = GitRepository.of(url)
           expect(repo.host).toBe('git.example.com')
-          expect(repo.owner).toBe('munierujp')
+          expect(repo.owner).toBe('npm-update-package')
           expect(repo.name).toBe('npm-update-package')
           expect(repo.apiEndPoint).toBe('https://git.example.com/api/v3')
           expect(repo.isGitHubDotCom).toBe(false)
