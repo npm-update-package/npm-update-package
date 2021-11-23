bdc2fe465db675ea06034c617bae8d299eac17b1
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-23 17:50:42 +0900

    add Options.ts

---
+++ src/options/Options.ts
@@ -0,0 +1,24 @@
+import {
+  intersection,
+  literal,
+  partial,
+  string,
+  type,
+  union
+} from 'io-ts'
+import type { TypeOf } from 'io-ts'
+
+export const Options = intersection([
+  type({
+    githubToken: string,
+    logLevel: union([literal('info'), literal('debug')]),
+    packageManager: union([literal('npm'), literal('yarn')])
+  }),
+  partial({
+    gitUserEmail: string,
+    gitUserName: string
+  })
+])
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export type Options = TypeOf<typeof Options>
+export const isOptions = Options.is
