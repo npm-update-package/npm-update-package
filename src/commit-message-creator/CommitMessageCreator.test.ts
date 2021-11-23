a94d51ca6a8d28c473c40e08eff968d7ad0fb8e5
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-24 00:55:45 +0900

    add CommitMessageCreator

---
+++ src/commit-message-creator/CommitMessageCreator.test.ts
@@ -0,0 +1,17 @@
+import { PackageVersion } from '../values'
+import { CommitMessageCreator } from './CommitMessageCreator'
+
+describe('CommitMessageCreator', () => {
+  describe('create', () => {
+    it('returns commit message', () => {
+      const commitMessageCreator = new CommitMessageCreator('chore(deps): {{updateType}} update {{{packageName}}} from {{currentVersion}} to v{{newVersion}}')
+      const actual = commitMessageCreator.create({
+        name: '@typescript-eslint/eslint-plugin',
+        currentVersion: PackageVersion.of('1.0.0'),
+        newVersion: PackageVersion.of('2.0.0'),
+        type: 'major'
+      })
+      expect(actual).toBe('chore(deps): major update @typescript-eslint/eslint-plugin from 1.0.0 to v2.0.0')
+    })
+  })
+})
