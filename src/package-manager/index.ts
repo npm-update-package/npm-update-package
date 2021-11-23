4ac1ff0375b373fbf081bcd7e51b563502c93c61
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-23 19:43:00 +0900

    remove PackageManagerCreator.ts

--- src/package-manager/index.ts
+++ src/package-manager/index.ts
@@ -1,5 +1,4 @@
 export { createPackageManager } from './createPackageManager'
 export { Npm } from './Npm'
 export type { PackageManager } from './PackageManager'
-export { PackageManagerCreator } from './PackageManagerCreator'
 export { Yarn } from './Yarn'
