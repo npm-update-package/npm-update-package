c13fb95f741541fe13184005b21f50eaa2945886
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-24 21:46:11 +0900

    remove createPullRequestTitle.ts

--- src/github/createPullRequestTitle.ts
+++
@@ -1,8 +0,0 @@
-import type { OutdatedPackage } from '../ncu'
-
-export const createPullRequestTitle = (outdatedPackage: OutdatedPackage): string => {
-  const packageName = outdatedPackage.name
-  const newVersion = outdatedPackage.newVersion.version
-  const updateType = outdatedPackage.type
-  return `chore(deps): ${updateType} update ${packageName} to v${newVersion}`
-}
