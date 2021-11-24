1518d349822285d24a364b1fa691b3a6688667ce
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-24 19:57:10 +0900

    add PackageDependencies.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
 export type { OutdatedPackage } from './OutdatedPackage'
+export { isPackageDependencies } from './PackageDependencies'
+export type { PackageDependencies } from './PackageDependencies'
 export type { Result } from './Result'
