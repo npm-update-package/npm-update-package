1639d3a9e66d2e4956d9f5fc02fb1ab9cbd5a360
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-24 21:31:03 +0900

    add PullRequestTitleCreator

---
+++ src/github/PullRequestTitleCreator.test.ts
@@ -0,0 +1,17 @@
+import { PackageVersion } from '../ncu'
+import { PullRequestTitleCreator } from './PullRequestTitleCreator'
+
+describe('PullRequestTitleCreator', () => {
+  describe('create', () => {
+    it('returns commit message', () => {
+      const pullRequestTitleCreator = new PullRequestTitleCreator('chore(deps): {{updateType}} update {{{packageName}}} from {{currentVersion}} to v{{newVersion}}')
+      const actual = pullRequestTitleCreator.create({
+        name: '@typescript-eslint/eslint-plugin',
+        currentVersion: PackageVersion.of('1.0.0'),
+        newVersion: PackageVersion.of('2.0.0'),
+        type: 'major'
+      })
+      expect(actual).toBe('chore(deps): major update @typescript-eslint/eslint-plugin from 1.0.0 to v2.0.0')
+    })
+  })
+})
