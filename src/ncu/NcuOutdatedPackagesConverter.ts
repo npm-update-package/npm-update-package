eae7a4e6b44f7085fe2e4d751de8fb933934c981
Munieru <20086673+munierujp@users.noreply.github.com>
2021-11-24 20:02:42 +0900

    update NcuOutdatedPackagesConverter

--- src/ncu/NcuOutdatedPackagesConverter.ts
+++ src/ncu/NcuOutdatedPackagesConverter.ts
@@ -1,5 +1,7 @@
-import type { PackageDependencies } from '../package-json-reader'
-import type { OutdatedPackage } from '../types'
+import type {
+  OutdatedPackage,
+  PackageDependencies
+} from '../types'
 import { PackageVersion } from '../values'
 import type { NcuOutdatedPackages } from './NcuOutdatedPackages'
 import { toUpdateType } from './toUpdateType'
