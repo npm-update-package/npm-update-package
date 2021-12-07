d8eaef089604c60b426346b0c2078c4861b30810
Munieru <20086673+munierujp@users.noreply.github.com>
2021-12-08 01:07:57 +0900

    remove runWithErrorHandling

--- src/terminal/Terminal.ts
+++ src/terminal/Terminal.ts
@@ -1,6 +1,5 @@
 import execa from 'execa'
 import type { ExecaReturnValue } from 'execa'
-import { isExecaReturnValue } from './isExecaReturnValue'
 
 // TODO: add test
 export class Terminal {
@@ -9,22 +8,5 @@ export class Terminal {
     ...args: string[]
   ): Promise<ExecaReturnValue<string>> {
     return await execa(command, args)
-  }
-
-  async runWithErrorHandling (
-    command: string,
-    ...args: string[]
-  ): Promise<ExecaReturnValue<string>> {
-    try {
-      return await this.run(command, ...args)
-    } catch (e) {
-      const value: unknown = e instanceof Error ? JSON.parse(JSON.stringify(e)) : e
-
-      if (isExecaReturnValue(value)) {
-        return value
-      } else {
-        throw e
-      }
-    }
   }
 }
