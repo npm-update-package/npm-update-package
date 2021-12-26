327c871380638a060f3e0adce4c43dfa970480a1
Munieru <20086673+munierujp@users.noreply.github.com>
2021-12-27 00:37:39 +0900

    add --pull-request-labels option

--- src/options/Options.ts
+++ src/options/Options.ts
@@ -1,5 +1,8 @@
 import {
+  array,
+  intersection,
   literal,
+  partial,
   string,
   type,
   union,
@@ -8,15 +11,21 @@ import {
 import { LogLevel } from '../logger'
 import { PackageManagerName } from '../package-manager'
 
-const Options = type({
-  branchName: string,
-  commitMessage: string,
-  githubToken: string,
-  logLevel: union([literal(LogLevel.Off), literal(LogLevel.Error), literal(LogLevel.Info), literal(LogLevel.Debug)]),
-  packageManager: union([literal(PackageManagerName.Npm), literal(PackageManagerName.Yarn)]),
-  pullRequestBody: string,
-  pullRequestTitle: string
-})
+const Options = intersection([
+  type({
+    branchName: string,
+    commitMessage: string,
+    githubToken: string,
+    logLevel: union([literal(LogLevel.Off), literal(LogLevel.Error), literal(LogLevel.Info), literal(LogLevel.Debug)]),
+    packageManager: union([literal(PackageManagerName.Npm), literal(PackageManagerName.Yarn)]),
+    pullRequestBody: string,
+    pullRequestTitle: string
+  }),
+  partial({
+    pullRequestLabels: array(string)
+  })
+])
+
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Options = TypeOf<typeof Options>
 export const isOptions = Options.is
