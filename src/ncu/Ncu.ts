4c9605ed6b54eb0a13091c435f9ab94b4c99ecfc
Munieru <20086673+munierujp@users.noreply.github.com>
2021-12-31 14:32:06 +0900

    Refactor Ncu (#149)
    * refactor Ncu
    
    * update Ncu
    
    add logger

--- src/ncu/Ncu.ts
+++ src/ncu/Ncu.ts
@@ -1,13 +1,27 @@
 import { run } from 'npm-check-updates'
-import type { Options } from 'npm-check-updates/build/src/types'
+import type { Logger } from '../logger'
 import type { OutdatedPackage } from '../ncu'
 import type { PackageJsonReader } from '../package-json'
 import { isNcuResult } from './NcuResult'
 import { NcuResultConverter } from './NcuResultConverter'
 
+type Options = NonNullable<Parameters<typeof run>[0]>
+
 // TODO: add test
 export class Ncu {
-  constructor (private readonly packageJsonReader: PackageJsonReader) {}
+  private readonly packageJsonReader: PackageJsonReader
+  private readonly logger: Logger
+
+  constructor ({
+    packageJsonReader,
+    logger
+  }: {
+    packageJsonReader: PackageJsonReader
+    logger: Logger
+  }) {
+    this.packageJsonReader = packageJsonReader
+    this.logger = logger
+  }
 
   async check (): Promise<OutdatedPackage[]> {
     return await this.run({
@@ -25,19 +39,20 @@ export class Ncu {
 
   private async run (options: Options): Promise<OutdatedPackage[]> {
     const pkg = await this.packageJsonReader.read()
-    const currentDependencies = {
-      ...pkg.dependencies,
-      ...pkg.devDependencies,
-      ...pkg.peerDependencies,
-      ...pkg.optionalDependencies
-    }
+    this.logger.debug(`pkg=${JSON.stringify(pkg)}`)
     const result = await run(options)
+    this.logger.debug(`result=${JSON.stringify(result)}`)
 
     if (!isNcuResult(result)) {
       throw new Error('result is not NcuResult')
     }
 
-    const ncuResultConverter = new NcuResultConverter(currentDependencies)
+    const ncuResultConverter = new NcuResultConverter({
+      ...pkg.dependencies,
+      ...pkg.devDependencies,
+      ...pkg.peerDependencies,
+      ...pkg.optionalDependencies
+    })
     return ncuResultConverter.toOutdatedPackages(result)
   }
 }
