ef88eed6c07f141ed1f3b6df30a53538e424d33b
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-02 21:12:36 +0900

    Remove readFile.ts (#160)
    * update PackageJsonReader
    
    * remove readFile

--- src/file/readFile.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-
-export type Path = Parameters<typeof fs.promises.readFile>[0]
-
-// TODO: add test
-export const readFile = async (path: Path): Promise<string> => {
-  const data = await fs.promises.readFile(path, 'utf8')
-  return data
-}
