8b55f9acf60265137872c456b2882d6a826e6174
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-02 20:13:07 +0900

    fix errors

--- src/file/readFile.ts
+++ src/file/readFile.ts
@@ -1,9 +1,9 @@
-import { readFile as readFileAsync } from 'fs/promises'
+import fs from 'fs'
 
-export type Path = Parameters<typeof readFileAsync>[0]
+export type Path = Parameters<typeof fs.promises.readFile>[0]
 
 // TODO: add test
 export const readFile = async (path: Path): Promise<string> => {
-  const data = await readFileAsync(path, 'utf8')
+  const data = await fs.promises.readFile(path, 'utf8')
   return data
 }
