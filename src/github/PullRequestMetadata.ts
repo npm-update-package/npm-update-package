c3d93676fa5f8a2bfb71bf77d1959eb6cf8a9c61
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-09 02:08:28 +0900

    add PullRequestMetadata.ts

---
+++ src/github/PullRequestMetadata.ts
@@ -0,0 +1,22 @@
+import {
+  array,
+  literal,
+  string,
+  type,
+  union,
+  type TypeOf
+} from 'io-ts'
+import { UpdateType } from '../ncu'
+
+const PullRequestMetadata = type({
+  version: string,
+  packages: array(type({
+    name: string,
+    currentVersion: string,
+    newVersion: string,
+    type: union([literal(UpdateType.Major), literal(UpdateType.Minor), literal(UpdateType.Patch)])
+  }))
+})
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export type PullRequestMetadata = TypeOf<typeof PullRequestMetadata>
+export const isPullRequestMetadata = PullRequestMetadata.is
