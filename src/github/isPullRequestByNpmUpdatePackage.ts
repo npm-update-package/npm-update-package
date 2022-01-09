c9395e54141baef5a8ac5e373fbe47cf56ad40f7
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-09 17:53:21 +0900

    update isPullRequestByNpmUpdatePackage

--- src/github/isPullRequestByNpmUpdatePackage.ts
+++ src/github/isPullRequestByNpmUpdatePackage.ts
@@ -1,5 +1,5 @@
 import type { PullRequest } from './PullRequest'
 
 export const isPullRequestByNpmUpdatePackage = (pullRequest: PullRequest): boolean => {
-  return pullRequest.labels.map(({ name }) => name).includes('npm-update-package')
+  return pullRequest.labels.some(({ name }) => name === 'npm-update-package')
 }
