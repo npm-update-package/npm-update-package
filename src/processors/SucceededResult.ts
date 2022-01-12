0868d82746dd30c7f43b57c0bf519858e45840de
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-12 18:52:35 +0900

    add TODO comment

--- src/processors/SucceededResult.ts
+++ src/processors/SucceededResult.ts
@@ -2,6 +2,7 @@ import type { OutdatedPackage } from '../ncu'
 
 export interface SucceededResult {
   outdatedPackage: OutdatedPackage
+  // TODO: rename to created
   updated?: boolean
   skipped?: boolean
 }
