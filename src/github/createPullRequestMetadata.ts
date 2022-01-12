69addfc313f70ef37d0953b0deb0a83f86c36707
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-12 23:30:50 +0900

    add createPullRequestMetadata.ts

---
+++ src/github/createPullRequestMetadata.ts
@@ -0,0 +1,16 @@
+import { app } from '../app'
+import type { OutdatedPackage } from '../ncu'
+import type { PullRequestMetadata } from './PullRequestMetadata'
+
+// TODO: Add test
+export const createPullRequestMetadata = (outdatedPackages: OutdatedPackage[]): PullRequestMetadata => {
+  return {
+    version: app.version,
+    packages: outdatedPackages.map(({ name, currentVersion, newVersion, type }) => ({
+      name,
+      currentVersion: currentVersion.version,
+      newVersion: newVersion.version,
+      type
+    }))
+  }
+}
