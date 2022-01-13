9427be468044cfd36ffaf6d36c7fbb54654c1345
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-14 00:13:40 +0900

    update main

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,10 @@ import {
 import type { Logger } from './logger'
 import { Ncu } from './ncu'
 import type { Options } from './options'
-import { PackageJsonParser } from './package-json'
-import { PackageJsonReader } from './package-json/PackageJsonReader'
+import {
+  PackageJsonParser,
+  PackageJsonReader
+} from './package-json'
 import { createPackageManager } from './package-manager'
 import {
   OutdatedPackageProcessor,
