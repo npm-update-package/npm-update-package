fbcd892c3322c5339808baebded9cc6cf0e9ead6
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-15 21:51:49 +0900

    fix toJSON

--- src/json/toJSON.ts
+++ src/json/toJSON.ts
@@ -7,8 +7,8 @@ export const toJSON = (value: unknown, options?: Options): string => {
   const pretty = options?.pretty ?? false
 
   if (pretty) {
-    return JSON.stringify(value)
-  } else {
     return JSON.stringify(value, null, 2)
+  } else {
+    return JSON.stringify(value)
   }
 }
