f432e69cf3446f4caf70d146789d4c6a15f66843
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-15 22:17:27 +0900

    add createOutdatedPackages

--- src/ncu/Ncu.ts
+++ src/ncu/Ncu.ts
@@ -1,11 +1,18 @@
 import { run } from 'npm-check-updates'
 import type { RunOptions } from 'npm-check-updates/build/src/types'
+import { isNotUndefined } from 'type-guards'
 import type { OutdatedPackage } from '../core'
 import { readFile } from '../file'
 import type { Logger } from '../logger'
-import { parsePackageJson } from '../package-json'
-import { isNcuResult } from './NcuResult'
-import { NcuResultConverter } from './NcuResultConverter'
+import { PackageMetadata, parsePackageJson } from '../package-json'
+import {
+  compareSemVers,
+  SemVer
+} from '../semver'
+import {
+  isNcuResult,
+  type NcuResult
+} from './NcuResult'
 
 // TODO: add test
 export class Ncu {
@@ -27,8 +34,8 @@ export class Ncu {
 
   private async run (options: RunOptions): Promise<OutdatedPackage[]> {
     const json = await readFile('./package.json')
-    const pkg = parsePackageJson(json)
-    this.logger.debug(`pkg=${JSON.stringify(pkg)}`)
+    const packageMetadata = parsePackageJson(json)
+    this.logger.debug(`packageMetadata=${JSON.stringify(packageMetadata)}`)
     const result = await run(options)
     this.logger.debug(`result=${JSON.stringify(result)}`)
 
@@ -36,7 +43,62 @@ export class Ncu {
       throw new Error('result is not NcuResult')
     }
 
-    const ncuResultConverter = new NcuResultConverter(pkg)
-    return ncuResultConverter.toOutdatedPackages(result)
+    return this.createOutdatedPackages({
+      result,
+      packageMetadata
+    })
+  }
+
+  private createOutdatedPackages ({
+    result,
+    packageMetadata
+  }: {
+    result: NcuResult
+    packageMetadata: PackageMetadata
+  }): OutdatedPackage[] {
+    const {
+      dependencies,
+      devDependencies,
+      peerDependencies,
+      optionalDependencies
+    } = packageMetadata
+    const currentDependencies = {
+      ...dependencies,
+      ...devDependencies,
+      ...peerDependencies,
+      ...optionalDependencies
+    }
+    const resultEntries = Object.entries(result)
+    const outdatedPackages: OutdatedPackage[] = resultEntries
+      .map(([name, newVersionString]) => {
+        const currentVersionString = currentDependencies[name]
+
+        if (currentVersionString === undefined) {
+          return undefined
+        }
+
+        const currentVersion = SemVer.of(currentVersionString)
+        const newVersion = SemVer.of(newVersionString)
+        const level = compareSemVers(currentVersion, newVersion)
+
+        if (level === undefined) {
+          return undefined
+        }
+
+        const outdatedPackage: OutdatedPackage = {
+          name,
+          currentVersion,
+          newVersion,
+          level
+        }
+        return outdatedPackage
+      })
+      .filter(isNotUndefined)
+
+    if (resultEntries.length !== outdatedPackages.length) {
+      throw new Error('Failed to create outdatedPackages.')
+    }
+
+    return outdatedPackages
   }
 }
