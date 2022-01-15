951f60bf8b6d713791b8156db67ed0a76066d412
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-15 21:50:50 +0900

    beautify

--- src/json/toJSON.ts
+++ src/json/toJSON.ts
@@ -5,6 +5,7 @@ interface Options {
 // TODO: Add test
 export const toJSON = (value: unknown, options?: Options): string => {
   const pretty = options?.pretty ?? false
+
   if (pretty) {
     return JSON.stringify(value)
   } else {
