193ba2c2cc2b58c3ee073fc536ebd357d52a9880
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-16 16:35:06 +0900

    update SemVer.test.ts (#207)

--- src/semver/SemVer.test.ts
+++ src/semver/SemVer.test.ts
@@ -2,24 +2,16 @@ import { SemVer } from './SemVer'
 
 describe('SemVer', () => {
   describe('of', () => {
-    describe('if version is valid', () => {
-      const version = '^1.2.3'
-
-      it('returns new SemVer instance', () => {
-        const semver = SemVer.of(version)
-        expect(semver.version).toBe('1.2.3')
-        expect(semver.major).toBe(1)
-        expect(semver.minor).toBe(2)
-        expect(semver.patch).toBe(3)
-      })
+    it('returns new SemVer instance if version is valid', () => {
+      const semver = SemVer.of('^1.2.3')
+      expect(semver.version).toBe('1.2.3')
+      expect(semver.major).toBe(1)
+      expect(semver.minor).toBe(2)
+      expect(semver.patch).toBe(3)
     })
 
-    describe('if version is invalid', () => {
-      const version = ''
-
-      it('throws error', () => {
-        expect(() => SemVer.of(version)).toThrow(Error)
-      })
+    it('throws error if version is invalid', () => {
+      expect(() => SemVer.of('')).toThrow(Error)
     })
   })
 })
