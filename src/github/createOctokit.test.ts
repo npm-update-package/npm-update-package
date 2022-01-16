0af6ba4a2246411f58a3ee00233b9c1c66ad7879
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-16 19:04:18 +0900

    Update createOctokit.test.ts (#208)

--- src/github/createOctokit.test.ts
+++ src/github/createOctokit.test.ts
@@ -5,29 +5,23 @@ import { createOctokit } from './createOctokit'
 describe('createOctokit', () => {
   const token = 'token'
 
-  describe('if repository is GitHub.com', () => {
+  it('returns new Octokit instance for GitHub.com if repository is GitHub.com', () => {
     const repository = GitRepository.of('https://github.com/npm-update-package/npm-update-package.git')
-
-    it('returns new Octokit instance for GitHub.com', () => {
-      const octokit = createOctokit({
-        repository,
-        token
-      })
-
-      expect(octokit).toBeInstanceOf(Octokit)
+    const octokit = createOctokit({
+      repository,
+      token
     })
+
+    expect(octokit).toBeInstanceOf(Octokit)
   })
 
-  describe('if repository is not GitHub.com', () => {
+  it('returns new Octokit instance for GitHub Enterprise if repository is not GitHub.com', () => {
     const repository = GitRepository.of('https://git.example.com/npm-update-package/npm-update-package.git')
-
-    it('returns new Octokit instance for GitHub Enterprise', () => {
-      const octokit = createOctokit({
-        repository,
-        token
-      })
-
-      expect(octokit).toBeInstanceOf(Octokit)
+    const octokit = createOctokit({
+      repository,
+      token
     })
+
+    expect(octokit).toBeInstanceOf(Octokit)
   })
 })
