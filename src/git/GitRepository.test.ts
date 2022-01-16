e160768c1e186e8bf8fcabb7bd15d1c035474e14
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-16 21:49:44 +0900

    Update GitRepository.test.ts

--- src/git/GitRepository.test.ts
+++ src/git/GitRepository.test.ts
@@ -2,59 +2,85 @@ import { GitRepository } from './GitRepository'
 
 describe('GitRepository', () => {
   describe('of', () => {
-    describe('if URL is HTTPS', () => {
-      describe('if URL is github.com', () => {
-        const url = 'https://github.com/npm-update-package/npm-update-package.git'
+    describe('returns new GitRepository instance if URL is valid', () => {
+      interface TestCase {
+        url: string
+        host: string
+        owner: string
+        name: string
+        apiEndPoint: string
+        isGitHubDotCom: boolean
+      }
+      const cases: TestCase[] = [
+        {
+          url: 'https://github.com/npm-update-package/npm-update-package.git',
+          host: 'github.com',
+          owner: 'npm-update-package',
+          name: 'npm-update-package',
+          apiEndPoint: 'https://api.github.com',
+          isGitHubDotCom: true
+        },
+        {
+          url: 'git@github.com:npm-update-package/npm-update-package.git',
+          host: 'github.com',
+          owner: 'npm-update-package',
+          name: 'npm-update-package',
+          apiEndPoint: 'https://api.github.com',
+          isGitHubDotCom: true
+        },
+        {
+          url: 'https://git.example.com/npm-update-package/npm-update-package.git',
+          host: 'git.example.com',
+          owner: 'npm-update-package',
+          name: 'npm-update-package',
+          apiEndPoint: 'https://git.example.com/api/v3',
+          isGitHubDotCom: false
+        },
+        {
+          url: 'git@git.example.com:npm-update-package/npm-update-package.git',
+          host: 'git.example.com',
+          owner: 'npm-update-package',
+          name: 'npm-update-package',
+          apiEndPoint: 'https://git.example.com/api/v3',
+          isGitHubDotCom: false
+        }
+      ]
 
-        it('returns new GitRepository instance', () => {
-          const repo = GitRepository.of(url)
-          expect(repo.host).toBe('github.com')
-          expect(repo.owner).toBe('npm-update-package')
-          expect(repo.name).toBe('npm-update-package')
-          expect(repo.apiEndPoint).toBe('https://api.github.com')
-          expect(repo.isGitHubDotCom).toBe(true)
-        })
-      })
-
-      describe('if URL is not github.com', () => {
-        const url = 'https://git.example.com/npm-update-package/npm-update-package.git'
-
-        it('returns new GitRepository instance', () => {
-          const repo = GitRepository.of(url)
-          expect(repo.host).toBe('git.example.com')
-          expect(repo.owner).toBe('npm-update-package')
-          expect(repo.name).toBe('npm-update-package')
-          expect(repo.apiEndPoint).toBe('https://git.example.com/api/v3')
-          expect(repo.isGitHubDotCom).toBe(false)
-        })
+      it.each<TestCase>(cases)('url="$url"', ({
+        url,
+        host,
+        owner,
+        name,
+        apiEndPoint,
+        isGitHubDotCom
+      }) => {
+        const repo = GitRepository.of(url)
+        expect(repo).toBeInstanceOf(GitRepository)
+        expect(repo.host).toBe(host)
+        expect(repo.owner).toBe(owner)
+        expect(repo.name).toBe(name)
+        expect(repo.apiEndPoint).toBe(apiEndPoint)
+        expect(repo.isGitHubDotCom).toBe(isGitHubDotCom)
       })
     })
 
-    describe('if URL is SSH', () => {
-      describe('if URL is github.com', () => {
-        const url = 'git@github.com:npm-update-package/npm-update-package.git'
+    describe('throws error if URL is invalid', () => {
+      interface TestCase {
+        url: string
+      }
+      const cases: TestCase[] = [
+        {
+          url: ''
+        },
+        {
+          url: 'https://example.com/'
+        }
+      ]
 
-        it('returns new GitRepository instance', () => {
-          const repo = GitRepository.of(url)
-          expect(repo.host).toBe('github.com')
-          expect(repo.owner).toBe('npm-update-package')
-          expect(repo.name).toBe('npm-update-package')
-          expect(repo.apiEndPoint).toBe('https://api.github.com')
-          expect(repo.isGitHubDotCom).toBe(true)
-        })
-      })
-
-      describe('if URL is not github.com', () => {
-        const url = 'git@git.example.com:npm-update-package/npm-update-package.git'
-
-        it('returns new GitRepository instance', () => {
-          const repo = GitRepository.of(url)
-          expect(repo.host).toBe('git.example.com')
-          expect(repo.owner).toBe('npm-update-package')
-          expect(repo.name).toBe('npm-update-package')
-          expect(repo.apiEndPoint).toBe('https://git.example.com/api/v3')
-          expect(repo.isGitHubDotCom).toBe(false)
-        })
+      it.each<TestCase>(cases)('url="$url"', ({
+        url
+      }) => {
+        expect(() => GitRepository.of(url)).toThrow(Error)
       })
     })
   })
