c049aaec62691fab45382dad0bbf864befe822df
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-17 20:12:47 +0900

    add toJSON.test.ts

---
+++ src/json/toJSON.test.ts
@@ -0,0 +1,65 @@
+import { toJSON } from './toJSON'
+
+type TestCase = [unknown, string]
+
+describe('toJSON', () => {
+  describe('returns not indented JSON string if pretty option is false', () => {
+    const cases: TestCase[] = [
+      [
+        {
+          number: 1,
+          string: 'string'
+        },
+        '{"number":1,"string":"string"}'
+      ],
+      [
+        [
+          {
+            number: 1,
+            string: 'string'
+          }
+        ],
+        '[{"number":1,"string":"string"}]'
+      ]
+    ]
+
+    it.each<TestCase>(cases)('%j', (value, expected) => {
+      const json = toJSON(value)
+      expect(json).toBe(expected)
+    })
+  })
+
+  describe('returns indented JSON string if pretty option is true', () => {
+    const cases: TestCase[] = [
+      [
+        {
+          number: 1,
+          string: 'string'
+        },
+`{
+  "number": 1,
+  "string": "string"
+}`
+      ],
+      [
+        [
+          {
+            number: 1,
+            string: 'string'
+          }
+        ],
+`[
+  {
+    "number": 1,
+    "string": "string"
+  }
+]`
+      ]
+    ]
+
+    it.each<TestCase>(cases)('%j', (value, expected) => {
+      const json = toJSON(value, { pretty: true })
+      expect(json).toBe(expected)
+    })
+  })
+})
