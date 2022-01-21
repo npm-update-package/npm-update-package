80361d0726c36b409473983514a304b02c5c66a8
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-21 22:20:00 +0900

    Fix TODO comment

--- src/github/errors/NotFoundError.ts
+++ src/github/errors/NotFoundError.ts
@@ -5,7 +5,7 @@ export interface NotFoundError extends RequestError {
   status: StatusCodes.NOT_FOUND
 }
 
-// TODO: Add error
+// TODO: Add test
 export const isNotFoundError = (value: unknown): value is NotFoundError => {
   return value instanceof RequestError && value.status === StatusCodes.NOT_FOUND
 }
