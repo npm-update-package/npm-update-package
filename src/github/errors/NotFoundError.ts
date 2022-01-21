c3e1f3ec4725ec341d89d986e905b5736568614a
Munieru <20086673+munierujp@users.noreply.github.com>
2022-01-21 21:15:25 +0900

    Add NotFoundError.ts

---
+++ src/github/errors/NotFoundError.ts
@@ -0,0 +1,10 @@
+import { RequestError } from '@octokit/request-error'
+import { StatusCodes } from 'http-status-codes'
+
+export interface NotFoundError extends RequestError {
+  status: StatusCodes.NOT_FOUND
+}
+
+export const isNotFoundError = (value: unknown): value is NotFoundError => {
+  return value instanceof RequestError && value.status === StatusCodes.NOT_FOUND
+}
--- src/github/index.ts
+++ src/github/index.ts
@@ -1,3 +1,4 @@
 export * from './branch'
+export * from './errors'
 export * from './createGitHub'
 export * from './pull-request'
