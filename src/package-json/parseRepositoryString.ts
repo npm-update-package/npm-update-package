353e173636f40341e56516c81e3af023122e69cf
Munieru <20086673+munierujp@users.noreply.github.com>
2022-02-23 00:13:53 +0900

    Update comment

--- src/package-json/parseRepositoryString.ts
+++ src/package-json/parseRepositoryString.ts
@@ -11,7 +11,7 @@ export const parseRepositoryString = (repository: string): {
 
   const [, , type, owner, repo] = matched
 
-  /* istanbul ignore if: I can't write a test to reach here.  */
+  /* istanbul ignore if: I can't write a test to reach here. */
   if (owner === undefined || repo === undefined) {
     throw new Error(`Failed to parse repository. repository=${repository}`)
   }
