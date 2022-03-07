1874657a4f7a73cffb7c19b96a0bfe3e872feba6
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-07 19:15:12 +0900

    Remove TODO comment

--- src/package-manager/Npm.ts
+++ src/package-manager/Npm.ts
@@ -1,7 +1,6 @@
 import type { Terminal } from '../terminal'
 import type { PackageManager } from './PackageManager'
 
-// TODO: add test
 export class Npm implements PackageManager {
   readonly packageFile = 'package.json'
   readonly lockFile = 'package-lock.json'
