68b1b92b2e60c3d354122c268749cb95cf4ea35f
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-08 23:33:45 +0900

    Refactor extractPullRequestMetadata.ts (#338)

--- src/github/pull-request/metadata/extractPullRequestMetadata.ts
+++ src/github/pull-request/metadata/extractPullRequestMetadata.ts
@@ -5,8 +5,14 @@ import {
 
 export const extractPullRequestMetadata = (pullRequestBody: string): PullRequestMetadata | undefined => {
   const matched = pullRequestBody.match(/<div id="npm-update-package-metadata">\s*```json\s*([\s\S]+?)\s*```\s*<\/div>/)
-  const json = matched?.[1]
 
+  if (matched === null) {
+    return undefined
+  }
+
+  const json = matched[1]
+
+  /* istanbul ignore if: I can't write a test to reach here. */
   if (json === undefined) {
     return undefined
   }
