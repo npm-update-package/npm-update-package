fe0b855f8e443c2f26bb62b3945e58d593a3ec80
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-09 22:03:22 +0900

    Add PullRequestFinder.test.ts

---
+++ src/github/pull-request/finder/PullRequestFinder.test.ts
@@ -0,0 +1,86 @@
+import type { PullRequest } from '../../GitHub'
+import { PullRequestFinder } from './PullRequestFinder'
+
+describe('PullRequestFinder', () => {
+  describe('findByPackageName', () => {
+    const expected = [
+      {
+        labels: [
+          {
+            name: 'npm-update-package'
+          }
+        ],
+        body: `<div id="npm-update-package-metadata">
+  
+  \`\`\`json
+  {
+  "version": "1.0.0",
+  "packages": [
+    {
+      "name": "@npm-update-package/foo",
+      "currentVersion": "1.0.0",
+      "newVersion": "2.0.0",
+      "level": "major"
+    }
+  ]
+  }
+  \`\`\`
+  
+  </div>`
+      }
+    ] as unknown as PullRequest[]
+    const pullRequests = [
+      ...expected,
+      {
+        labels: [
+          {
+            name: 'npm-update-package'
+          }
+        ],
+        body: `<div id="npm-update-package-metadata">
+
+\`\`\`json
+{
+  "version": "1.0.0",
+  "packages": [
+    {
+      "name": "@npm-update-package/bar",
+      "currentVersion": "1.0.0",
+      "newVersion": "2.0.0",
+      "level": "major"
+    }
+  ]
+}
+\`\`\`
+
+</div>`
+      },
+      {
+        labels: [
+          {
+            name: 'npm-update-package'
+          }
+        ],
+        body: ''
+      },
+      {
+        labels: [
+          {
+            name: 'npm-update-package'
+          }
+        ],
+        body: null
+      },
+      {
+        labels: [],
+        body: null
+      }
+    ] as unknown as PullRequest[]
+    const pullRequestFinder = new PullRequestFinder(pullRequests)
+
+    it('returns pull requests for specified package', () => {
+      const actual = pullRequestFinder.findByPackageName('@npm-update-package/foo')
+      expect(actual).toEqual(expected)
+    })
+  })
+})
