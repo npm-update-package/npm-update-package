0405da23d7c4e6fff33b106a1964d3c2beb7105a
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-12 11:56:54 +0900

    Add Versions.test.ts

--- src/package-manager/Versions.ts
+++ src/package-manager/Versions.ts
@@ -7,5 +7,4 @@ import {
 export const Versions = array(string)
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Versions = TypeOf<typeof Versions>
-// TODO: Add test
 export const isVersions = Versions.is
