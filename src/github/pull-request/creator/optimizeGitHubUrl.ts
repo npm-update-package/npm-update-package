bb839b71b2019468805e466916af485b47521335
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-12 21:37:40 +0900

    Update comment

--- src/github/pull-request/creator/optimizeGitHubUrl.ts
+++ src/github/pull-request/creator/optimizeGitHubUrl.ts
@@ -1,9 +1,11 @@
 import { URL } from 'url'
 
+/**
+ * Convert URL to prevent linking to other repositories.
+ */
 export const optimizeGitHubUrl = (url: string): string => {
   const newUrl = new URL(url)
 
-  // NOTE: Prevent linking to other repositories.
   if (newUrl.host === 'github.com') {
     newUrl.host = 'togithub.com'
   }
