48c121f937430ea5a4abfc82747ab4109f3ced9c
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-16 23:26:00 +0900

    Add createLogger.test.ts

--- src/logger/createLogger.ts
+++ src/logger/createLogger.ts
@@ -2,7 +2,6 @@ import { getLogger } from 'log4js'
 import type { Logger } from './Logger'
 import type { LogLevel } from './LogLevel'
 
-// TODO: add test
 export const createLogger = (logLevel: LogLevel): Logger => {
   const logger = getLogger()
   logger.level = logLevel
