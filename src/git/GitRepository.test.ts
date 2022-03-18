c03566cf6d0c6ba73756b1725caa79ec90e249ce
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-19 01:52:32 +0900

    Update GitRepository.test.ts

--- src/git/GitRepository.test.ts
+++ src/git/GitRepository.test.ts
@@ -3,59 +3,75 @@ import { GitRepository } from './GitRepository'
 
 describe('GitRepository', () => {
   describe('of', () => {
-    describe('returns new GitRepository instance if URL is valid', () => {
-      type TestCase = [
-        string,
-        {
+    describe('returns new GitRepository instance if repository is valid', () => {
+      interface TestCase {
+        repository: string
+        expected: {
           url: URL
           owner: string
           name: string
         }
-      ]
+      }
       const cases: TestCase[] = [
-        [
-          'npm-update-package/example',
-          {
+        {
+          repository: 'npm-update-package/example',
+          expected: {
             url: new URL('https://github.com/npm-update-package/example'),
             owner: 'npm-update-package',
             name: 'example'
           }
-        ],
-        [
-          'https://github.com/npm-update-package/example',
-          {
+        },
+        {
+          repository: 'https://github.com/npm-update-package/example',
+          expected: {
             url: new URL('https://github.com/npm-update-package/example'),
             owner: 'npm-update-package',
             name: 'example'
           }
-        ],
-        [
-          'https://github.com/npm-update-package/example.git',
-          {
+        },
+        {
+          repository: 'https://github.com/npm-update-package/example.git',
+          expected: {
             url: new URL('https://github.com/npm-update-package/example'),
             owner: 'npm-update-package',
             name: 'example'
           }
-        ],
-        [
-          'https://git.example.com/npm-update-package/example',
-          {
-            url: new URL('https://git.example.com/npm-update-package/example'),
-            owner: 'npm-update-package',
-            name: 'example'
-          }
-        ],
-        [
-          'git@github.com:npm-update-package/example.git',
-          {
+        },
+        {
+          repository: 'git@github.com:npm-update-package/example.git',
+          expected: {
             url: new URL('https://github.com/npm-update-package/example'),
             owner: 'npm-update-package',
             name: 'example'
           }
-        ]
+        },
+        {
+          repository: 'https://git.test/npm-update-package/example',
+          expected: {
+            url: new URL('https://git.test/npm-update-package/example'),
+            owner: 'npm-update-package',
+            name: 'example'
+          }
+        },
+        {
+          repository: 'https://git.test/npm-update-package/example.git',
+          expected: {
+            url: new URL('https://git.test/npm-update-package/example'),
+            owner: 'npm-update-package',
+            name: 'example'
+          }
+        },
+        {
+          repository: 'git@git.test:npm-update-package/example.git',
+          expected: {
+            url: new URL('https://git.test/npm-update-package/example'),
+            owner: 'npm-update-package',
+            name: 'example'
+          }
+        }
       ]
 
-      it.each<TestCase>(cases)('repository=%p', (repository, expected) => {
+      it.each<TestCase>(cases)('repository=$repository', ({ repository, expected }) => {
         const actual = GitRepository.of(repository)
         expect(actual).toBeInstanceOf(GitRepository)
         expect(actual.url).toEqual(expected.url)
@@ -64,11 +80,14 @@ describe('GitRepository', () => {
       })
     })
 
-    describe('throws error if URL is invalid', () => {
+    describe('throws error if repository is invalid', () => {
       type TestCase = string
       const cases: TestCase[] = [
         '',
-        'https://example.com/'
+        'https://github.com/',
+        'git@github.com',
+        'https://git.test/',
+        'git@git.test'
       ]
 
       it.each<TestCase>(cases)('repository=%p', (repository) => {
