a5653c821fed458efe8ca34fbb1b67a02bb9f62e
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-19 19:32:15 +0900

    Update extractRepository

--- src/package-json/extractRepository.test.ts
+++ src/package-json/extractRepository.test.ts
@@ -4,31 +4,20 @@ import { extractRepository } from './extractRepository'
 import type { PackageMetadata } from './PackageMetadata'
 
 describe('extractRepository', () => {
-  describe('if repository is string', () => {
-    it('returns GitRepository instance if repository is GitHub', () => {
-      const metadata: PackageMetadata = {
-        name: '@npm-update-package/example',
-        version: '1.0.0',
-        repository: 'npm-update-package/example'
-      }
+  it('returns GitRepository instance if repository is string', () => {
+    const metadata: PackageMetadata = {
+      name: '@npm-update-package/example',
+      version: '1.0.0',
+      repository: 'npm-update-package/example'
+    }
 
-      const actual = extractRepository(metadata)
+    const actual = extractRepository(metadata)
 
-      expect(actual).toBeInstanceOf(GitRepository)
-      expect(actual?.url).toEqual(new URL('https://github.com/npm-update-package/example'))
-      expect(actual?.owner).toBe('npm-update-package')
-      expect(actual?.name).toBe('example')
-    })
-
-    it('returns undefined if repository is not GitHub', () => {
-      const metadata: PackageMetadata = {
-        name: '@npm-update-package/example',
-        version: '1.0.0',
-        repository: 'gitlab:npm-update-package/example'
-      }
-
-      expect(extractRepository(metadata)).toBeUndefined()
-    })
+    expect(actual).toBeDefined()
+    expect(actual).toBeInstanceOf(GitRepository)
+    expect(actual?.url).toEqual(new URL('https://github.com/npm-update-package/example'))
+    expect(actual?.owner).toBe('npm-update-package')
+    expect(actual?.name).toBe('example')
   })
 
   it('returns GitRepository instance if repository is object', () => {
@@ -42,6 +31,7 @@ describe('extractRepository', () => {
 
     const actual = extractRepository(metadata)
 
+    expect(actual).toBeDefined()
     expect(actual).toBeInstanceOf(GitRepository)
     expect(actual?.url).toEqual(new URL('https://github.com/npm-update-package/example'))
     expect(actual?.owner).toBe('npm-update-package')
@@ -53,6 +43,9 @@ describe('extractRepository', () => {
       name: '@npm-update-package/example',
       version: '1.0.0'
     }
-    expect(extractRepository(metadata)).toBeUndefined()
+
+    const actual = extractRepository(metadata)
+
+    expect(actual).toBeUndefined()
   })
 })
--- src/package-json/extractRepository.ts
+++ src/package-json/extractRepository.ts
@@ -1,19 +1,13 @@
 import { GitRepository } from '../git'
 import type { PackageMetadata } from './PackageMetadata'
 
-export const extractRepository = ({
-  repository
-}: PackageMetadata): GitRepository | undefined => {
+export const extractRepository = (pkg: PackageMetadata): GitRepository | undefined => {
+  const { repository } = pkg
+
   if (repository === undefined) {
     return undefined
   }
 
   const repositoryString = typeof repository === 'string' ? repository : repository.url
-  const gitRepo = GitRepository.of(repositoryString)
-
-  if (gitRepo === undefined || !gitRepo.isGitHub) {
-    return undefined
-  }
-
-  return gitRepo
+  return GitRepository.of(repositoryString)
 }
