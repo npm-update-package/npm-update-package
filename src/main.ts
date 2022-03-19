44017c826baed21137e0d6620c80a3e54d915f55
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-19 19:27:12 +0900

    Update main

--- src/main.ts
+++ src/main.ts
@@ -62,7 +62,7 @@ export const main = async ({
   const gitRepo = GitRepository.of(remoteUrl)
   logger.debug(`gitRepo=${JSON.stringify(gitRepo)}`)
 
-  if (gitRepo === undefined) {
+  if (gitRepo === undefined || !gitRepo.isGitHub) {
     throw new Error(`Failed to parse remote url. URL=${remoteUrl}`)
   }
 
