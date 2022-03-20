4f20ed3fa15bacb5b363de65a771fe70977c9f97
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-20 18:49:12 +0900

    Add createOptions.ts

---
+++ src/options/createOptions.ts
@@ -0,0 +1,24 @@
+import { program } from 'commander'
+import pkg from '../../package.json'
+import type { CLIOption } from './CLIOption'
+import {
+  isOptions,
+  type Options
+} from './Options'
+import { toCommanderOption } from './toCommanderOption'
+
+// TODO: add test
+export const createOptions = (cliOptions: CLIOption[]): Options => {
+  program.version(pkg.version)
+  cliOptions
+    .map(toCommanderOption)
+    .forEach(option => program.addOption(option))
+  program.parse(process.argv)
+  const options = program.opts()
+
+  if (!isOptions(options)) {
+    throw new Error(`Failed to parse command-line options. options=${JSON.stringify(options)}`)
+  }
+
+  return options
+}
