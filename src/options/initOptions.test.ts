af17d69c7b70a3ce4e84e6d866aa5aa7bc78519d
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-20 18:56:34 +0900

    Add initOptions.test.ts

---
+++ src/options/initOptions.test.ts
@@ -0,0 +1,35 @@
+import { LogLevel } from '../logger'
+import { PackageManagerName } from '../package-manager'
+import { cliOptions } from './cliOptions'
+import { createOptions } from './createOptions'
+import { initOptions } from './initOptions'
+import type { Options } from './Options'
+
+jest.mock('./createOptions')
+
+describe('initOptions', () => {
+  const createOptionsMock = jest.mocked(createOptions)
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('returns Options', () => {
+    const expected: Options = {
+      commitMessage: 'test commitMessage',
+      githubToken: 'test githubToken',
+      logLevel: LogLevel.Info,
+      packageManager: PackageManagerName.Npm,
+      prTitle: 'test prTitle',
+      ignorePackages: ['@npm-update-package/example'],
+      prBodyNotes: 'test prBodyNotes',
+      reviewers: ['npm-update-package-bot']
+    }
+    createOptionsMock.mockReturnValue(expected)
+
+    const actual = initOptions()
+
+    expect(actual).toBe(expected)
+    expect(createOptionsMock).toBeCalledWith(cliOptions)
+  })
+})
--- src/options/initOptions.ts
+++ src/options/initOptions.ts
@@ -2,7 +2,6 @@ import { cliOptions } from './cliOptions'
 import { createOptions } from './createOptions'
 import type { Options } from './Options'
 
-// TODO: add test
 export const initOptions = (): Options => {
   return createOptions(cliOptions)
 }
