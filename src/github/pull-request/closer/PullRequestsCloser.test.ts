464d8a6dd28b16acc2f5c3a13891184d735b3282
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-20 14:26:42 +0900

    Add PullRequestsCloser.test.ts

---
+++ src/github/pull-request/closer/PullRequestsCloser.test.ts
@@ -0,0 +1,40 @@
+import {
+  createLogger,
+  LogLevel
+} from '../../../logger'
+import type { PullRequest } from '../../GitHub'
+import type { PullRequestCloser } from './PullRequestCloser'
+import { PullRequestsCloser } from './PullRequestsCloser'
+
+describe('PullRequestsCloser', () => {
+  describe('close', () => {
+    const pullRequestCloserCloseMock = jest.fn()
+    const pullRequestCloser = {
+      close: pullRequestCloserCloseMock
+    } as unknown as PullRequestCloser
+    const logger = createLogger(LogLevel.Off)
+    const pullRequestsCreator = new PullRequestsCloser({
+      pullRequestCloser,
+      logger
+    })
+
+    afterEach(() => {
+      jest.resetAllMocks()
+    })
+
+    it('closes pull requests', async () => {
+      const pullRequest1 = {
+        number: 1
+      } as unknown as PullRequest
+      const pullRequest2 = {
+        number: 2
+      } as unknown as PullRequest
+
+      await pullRequestsCreator.close([pullRequest1, pullRequest2])
+
+      expect(pullRequestCloserCloseMock).toBeCalledTimes(2)
+      expect(pullRequestCloserCloseMock).toBeCalledWith(pullRequest1)
+      expect(pullRequestCloserCloseMock).toBeCalledWith(pullRequest2)
+    })
+  })
+})
