21e39f5293637a2a148fe4c622f9985509a5e14c
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-21 18:51:05 +0900

    Update NpmVersions.test.ts

--- src/package-manager/NpmVersions.test.ts
+++ src/package-manager/NpmVersions.test.ts
@@ -4,16 +4,21 @@ import {
 } from './NpmVersions'
 
 describe('isNpmVersions', () => {
-  const versions: NpmVersions = [
-    '1.0.0',
-    '2.0.0'
-  ]
+  describe('returns whether value is NpmVersions', () => {
+    type TestCase = [unknown, boolean]
+    const versions: NpmVersions = [
+      '1.0.0',
+      '2.0.0'
+    ]
+    const cases: TestCase[] = [
+      [versions, true],
+      [{ data: versions }, false]
+    ]
 
-  it('returns true if value is NpmVersions', () => {
-    expect(isNpmVersions(versions)).toBe(true)
-  })
+    it.each(cases)('value=%p', (value, expected) => {
+      const actual = isNpmVersions(value)
 
-  it('returns false if value is not NpmVersions', () => {
-    expect(isNpmVersions(JSON.stringify(versions))).toBe(false)
+      expect(actual).toBe(expected)
+    })
   })
 })
