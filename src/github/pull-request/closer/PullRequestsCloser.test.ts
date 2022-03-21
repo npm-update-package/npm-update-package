f6c567c3d06760f90b23e60ec1267a92229af30a
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-21 21:26:48 +0900

    Update PullRequestsCloser.test.ts

--- src/github/pull-request/closer/PullRequestsCloser.test.ts
+++ src/github/pull-request/closer/PullRequestsCloser.test.ts
@@ -13,7 +13,7 @@ describe('PullRequestsCloser', () => {
       close: pullRequestCloserCloseMock
     } as unknown as PullRequestCloser
     const logger = createLogger(LogLevel.Off)
-    const pullRequestsCreator = new PullRequestsCloser({
+    const pullRequestsCloser = new PullRequestsCloser({
       pullRequestCloser,
       logger
     })
@@ -31,7 +31,7 @@ describe('PullRequestsCloser', () => {
       } as unknown as PullRequest
       const pullRequests = [pullRequest1, pullRequest2]
 
-      await pullRequestsCreator.close(pullRequests)
+      await pullRequestsCloser.close(pullRequests)
 
       expect(pullRequestCloserCloseMock).toBeCalledTimes(2)
       expect(pullRequestCloserCloseMock).toBeCalledWith(pullRequest1)
