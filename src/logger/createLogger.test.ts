089cdb7b5d617d5dd1015f996e75b4bd96a12760
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-21 11:11:02 +0900

    Update createLogger.test.ts

--- src/logger/createLogger.test.ts
+++ src/logger/createLogger.test.ts
@@ -5,95 +5,104 @@ describe('createLogger', () => {
   describe('returns new Logger instance', () => {
     interface TestCase {
       logLevel: LogLevel
-      isFatalEnabled: boolean
-      isErrorEnabled: boolean
-      isWarnEnabled: boolean
-      isInfoEnabled: boolean
-      isDebugEnabled: boolean
-      isTraceEnabled: boolean
+      expected: {
+        isFatalEnabled: boolean
+        isErrorEnabled: boolean
+        isWarnEnabled: boolean
+        isInfoEnabled: boolean
+        isDebugEnabled: boolean
+        isTraceEnabled: boolean
+      }
     }
     const cases: TestCase[] = [
       {
         logLevel: LogLevel.Off,
-        isFatalEnabled: false,
-        isErrorEnabled: false,
-        isWarnEnabled: false,
-        isInfoEnabled: false,
-        isDebugEnabled: false,
-        isTraceEnabled: false
+        expected: {
+          isFatalEnabled: false,
+          isErrorEnabled: false,
+          isWarnEnabled: false,
+          isInfoEnabled: false,
+          isDebugEnabled: false,
+          isTraceEnabled: false
+        }
       },
       {
         logLevel: LogLevel.Fatal,
-        isFatalEnabled: true,
-        isErrorEnabled: false,
-        isWarnEnabled: false,
-        isInfoEnabled: false,
-        isDebugEnabled: false,
-        isTraceEnabled: false
+        expected: {
+          isFatalEnabled: true,
+          isErrorEnabled: false,
+          isWarnEnabled: false,
+          isInfoEnabled: false,
+          isDebugEnabled: false,
+          isTraceEnabled: false
+        }
       },
       {
         logLevel: LogLevel.Error,
-        isFatalEnabled: true,
-        isErrorEnabled: true,
-        isWarnEnabled: false,
-        isInfoEnabled: false,
-        isDebugEnabled: false,
-        isTraceEnabled: false
+        expected: {
+          isFatalEnabled: true,
+          isErrorEnabled: true,
+          isWarnEnabled: false,
+          isInfoEnabled: false,
+          isDebugEnabled: false,
+          isTraceEnabled: false
+        }
       },
       {
         logLevel: LogLevel.Warn,
-        isFatalEnabled: true,
-        isErrorEnabled: true,
-        isWarnEnabled: true,
-        isInfoEnabled: false,
-        isDebugEnabled: false,
-        isTraceEnabled: false
+        expected: {
+          isFatalEnabled: true,
+          isErrorEnabled: true,
+          isWarnEnabled: true,
+          isInfoEnabled: false,
+          isDebugEnabled: false,
+          isTraceEnabled: false
+        }
       },
       {
         logLevel: LogLevel.Info,
-        isFatalEnabled: true,
-        isErrorEnabled: true,
-        isWarnEnabled: true,
-        isInfoEnabled: true,
-        isDebugEnabled: false,
-        isTraceEnabled: false
+        expected: {
+          isFatalEnabled: true,
+          isErrorEnabled: true,
+          isWarnEnabled: true,
+          isInfoEnabled: true,
+          isDebugEnabled: false,
+          isTraceEnabled: false
+        }
       },
       {
         logLevel: LogLevel.Debug,
-        isFatalEnabled: true,
-        isErrorEnabled: true,
-        isWarnEnabled: true,
-        isInfoEnabled: true,
-        isDebugEnabled: true,
-        isTraceEnabled: false
+        expected: {
+          isFatalEnabled: true,
+          isErrorEnabled: true,
+          isWarnEnabled: true,
+          isInfoEnabled: true,
+          isDebugEnabled: true,
+          isTraceEnabled: false
+        }
       },
       {
         logLevel: LogLevel.Trace,
-        isFatalEnabled: true,
-        isErrorEnabled: true,
-        isWarnEnabled: true,
-        isInfoEnabled: true,
-        isDebugEnabled: true,
-        isTraceEnabled: true
+        expected: {
+          isFatalEnabled: true,
+          isErrorEnabled: true,
+          isWarnEnabled: true,
+          isInfoEnabled: true,
+          isDebugEnabled: true,
+          isTraceEnabled: true
+        }
       }
     ]
 
-    it.each<TestCase>(cases)('logLevel=$logLevel', ({
-      logLevel,
-      isFatalEnabled,
-      isErrorEnabled,
-      isWarnEnabled,
-      isInfoEnabled,
-      isDebugEnabled,
-      isTraceEnabled
-    }) => {
-      const logger = createLogger(logLevel)
-      expect(logger.isFatalEnabled()).toBe(isFatalEnabled)
-      expect(logger.isErrorEnabled()).toBe(isErrorEnabled)
-      expect(logger.isWarnEnabled()).toBe(isWarnEnabled)
-      expect(logger.isInfoEnabled()).toBe(isInfoEnabled)
-      expect(logger.isDebugEnabled()).toBe(isDebugEnabled)
-      expect(logger.isTraceEnabled()).toBe(isTraceEnabled)
+    it.each(cases)('logLevel=$logLevel', ({ logLevel, expected }) => {
+      const actual = createLogger(logLevel)
+
+      expect(actual.isFatalEnabled()).toBe(expected.isFatalEnabled)
+      expect(actual.isErrorEnabled()).toBe(expected.isErrorEnabled)
+      expect(actual.isWarnEnabled()).toBe(expected.isWarnEnabled)
+      expect(actual.isInfoEnabled()).toBe(expected.isInfoEnabled)
+      expect(actual.isDebugEnabled()).toBe(expected.isDebugEnabled)
+      expect(actual.isTraceEnabled()).toBe(expected.isTraceEnabled)
     })
   })
 })
