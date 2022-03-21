7db27787acae06a9d722232ce946e1e46f847b69
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-21 10:42:50 +0900

    Update createBranchName.test.ts

--- src/git/createBranchName.test.ts
+++ src/git/createBranchName.test.ts
@@ -1,3 +1,4 @@
+import type { OutdatedPackage } from '../core'
 import { DependencyType } from '../package-json'
 import {
   SemVer,
@@ -7,13 +8,16 @@ import { createBranchName } from './createBranchName'
 
 describe('createBranchName', () => {
   it('returns branch name', () => {
-    const actual = createBranchName({
+    const outdatedPackage: OutdatedPackage = {
       name: '@npm-update-package/example',
       currentVersion: SemVer.of('1.0.0'),
       newVersion: SemVer.of('1.2.3'),
       level: SemVerLevel.Major,
       dependencyType: DependencyType.Dependencies
-    })
+    }
+
+    const actual = createBranchName(outdatedPackage)
+
     expect(actual).toBe('npm-update-package/@npm-update-package/example/v1.2.3')
   })
 })
