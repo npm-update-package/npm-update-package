32c1476c2633d0a0884e07c8ce3137c0f5c52aa8
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-22 21:13:09 +0900

    Revert "Delete OutdatedPackagesProcessor.test.ts (#414)" (#415)
    This reverts commit 909ea5cadfe3377f7bfd6236c589ce9c7ce5e964.

---
+++ src/core/OutdatedPackagesProcessor.test.ts
@@ -0,0 +1,99 @@
+import {
+  left,
+  right,
+  type Either
+} from 'fp-ts/lib/Either'
+import {
+  createLogger,
+  LogLevel
+} from '../logger'
+import { DependencyType } from '../package-json'
+import {
+  SemVer,
+  SemVerLevel
+} from '../semver'
+import type { FailedResult } from './FailedResult'
+import type { OutdatedPackage } from './OutdatedPackage'
+import type { OutdatedPackageProcessor } from './OutdatedPackageProcessor'
+import { OutdatedPackagesProcessor } from './OutdatedPackagesProcessor'
+import type { SucceededResult } from './SucceededResult'
+
+describe('OutdatedPackagesProcessor', () => {
+  describe('process', () => {
+    const outdatedPackageProcessorProcessMock = jest.fn()
+    const outdatedPackageProcessor = {
+      process: outdatedPackageProcessorProcessMock
+    } as unknown as OutdatedPackageProcessor
+    const logger = createLogger(LogLevel.Off)
+    const outdatedPackagesProcessor = new OutdatedPackagesProcessor({
+      outdatedPackageProcessor,
+      logger
+    })
+
+    afterEach(() => {
+      jest.resetAllMocks()
+    })
+
+    it('calls OutdatedPackageProcessor.process() by each packages', async () => {
+      const packageToBeCreated: OutdatedPackage = {
+        name: '@npm-update-package/example-1',
+        currentVersion: SemVer.of('1.0.0'),
+        newVersion: SemVer.of('2.0.0'),
+        level: SemVerLevel.Major,
+        dependencyType: DependencyType.Dependencies
+      }
+      const createdResult: Either<FailedResult, SucceededResult> = right({
+        outdatedPackage: packageToBeCreated,
+        created: true
+      })
+      const packageToBeSkipped: OutdatedPackage = {
+        name: '@npm-update-package/example-2',
+        currentVersion: SemVer.of('1.0.0'),
+        newVersion: SemVer.of('1.1.0'),
+        level: SemVerLevel.Minor,
+        dependencyType: DependencyType.Dependencies
+      }
+      const skippedResult: Either<FailedResult, SucceededResult> = right({
+        outdatedPackage: packageToBeSkipped,
+        skipped: true
+      })
+      const packageToBeFailed: OutdatedPackage = {
+        name: '@npm-update-package/example-3',
+        currentVersion: SemVer.of('1.0.0'),
+        newVersion: SemVer.of('1.0.1'),
+        level: SemVerLevel.Patch,
+        dependencyType: DependencyType.DevDependencies
+      }
+      const failedResult: Either<FailedResult, SucceededResult> = left({
+        outdatedPackage: packageToBeFailed,
+        error: new Error('Failed to update package')
+      })
+      outdatedPackageProcessorProcessMock.mockImplementation((outdatedPackage: OutdatedPackage) => {
+        switch (outdatedPackage.name) {
+          case packageToBeCreated.name:
+            return createdResult
+          case packageToBeSkipped.name:
+            return skippedResult
+          case packageToBeFailed.name:
+            return failedResult
+        }
+      })
+
+      const actual = await outdatedPackagesProcessor.process([
+        packageToBeCreated,
+        packageToBeSkipped,
+        packageToBeFailed
+      ])
+
+      expect(actual).toEqual([
+        createdResult,
+        skippedResult,
+        failedResult
+      ])
+      expect(outdatedPackageProcessorProcessMock).toBeCalledTimes(3)
+      expect(outdatedPackageProcessorProcessMock).toBeCalledWith(packageToBeCreated)
+      expect(outdatedPackageProcessorProcessMock).toBeCalledWith(packageToBeSkipped)
+      expect(outdatedPackageProcessorProcessMock).toBeCalledWith(packageToBeFailed)
+    })
+  })
+})
