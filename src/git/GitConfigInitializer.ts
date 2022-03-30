1a1cf377230bbc968fb86bd2696aed5dacac648c
Munieru <20086673+munierujp@users.noreply.github.com>
2022-03-31 00:56:05 +0900

    Add GitConfigInitializer

---
+++ src/git/GitConfigInitializer.ts
@@ -0,0 +1,28 @@
+import type { Options } from '../options'
+import type { Git } from './Git'
+
+export class GitConfigInitializer {
+  private readonly options: Options
+  private readonly git: Git
+
+  constructor ({
+    options,
+    git
+  }: {
+    options: Options
+    git: Git
+  }) {
+    this.options = options
+    this.git = git
+  }
+
+  async initialize (): Promise<void> {
+    if (this.options.gitUserName !== undefined) {
+      await this.git.setConfig('user.name', this.options.gitUserName)
+    }
+
+    if (this.options.gitUserEmail !== undefined) {
+      await this.git.setConfig('user.email', this.options.gitUserEmail)
+    }
+  }
+}
