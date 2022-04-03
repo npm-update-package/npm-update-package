b64425d95cdf7d2a81eea252ad0f222c1939c451
Munieru <20086673+munierujp@users.noreply.github.com>
2022-04-03 23:31:16 +0900

    Add detectPackageManager.test.ts

--- src/package-manager/detectPackageManager.ts
+++ src/package-manager/detectPackageManager.ts
@@ -1,7 +1,6 @@
 import { canReadWrite } from '../file'
 import { PackageManagerName } from './PackageManagerName'
 
-// TODO: Add test
 export const detectPackageManager = async (): Promise<PackageManagerName> => {
   if (await canReadWrite('package-lock.json')) {
     return PackageManagerName.Npm
