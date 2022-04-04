121fe16d134a2c0b7375702871565bf53f1931ba
Munieru <20086673+munierujp@users.noreply.github.com>
2022-04-05 00:54:12 +0900

    Add GitHubUrlOptimizer.ts

---
+++ src/github/pull-request/creator/GitHubUrlOptimizer.ts
@@ -0,0 +1,20 @@
+import { URL } from 'url'
+import type { Options } from '../../../options'
+
+// TODO: Add test
+export class GitHubUrlOptimizer {
+  constructor (private readonly options: Options) {}
+
+  /**
+   * Convert URL to prevent linking to other repositories.
+   */
+  optimize (url: string | URL): URL {
+    const newUrl = url instanceof URL ? url : new URL(url)
+
+    if (newUrl.host === 'github.com') {
+      newUrl.host = this.options.prBodyGithubHost
+    }
+
+    return newUrl
+  }
+}
