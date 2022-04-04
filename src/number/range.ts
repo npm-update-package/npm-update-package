fe0729b8c350012ee8a8c1ee8e45d3e1fdc6690b
Munieru <20086673+munierujp@users.noreply.github.com>
2022-04-04 09:48:09 +0900

    Beautify

--- src/number/range.ts
+++ src/number/range.ts
@@ -1,8 +1,5 @@
 // TODO: Add test
-export function * range (
-  start: number,
-  end: number
-): Generator<number, void, void> {
+export function * range (start: number, end: number): Generator<number, void, void> {
   for (let i = start; i <= end; i++) {
     yield i
   }
