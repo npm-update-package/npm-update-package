6e8581d41ca2da429b63e7009f41fd487cb28caa
Munieru <20086673+munierujp@users.noreply.github.com>
2022-04-05 02:14:22 +0900

    Add ReleaseNotesSectionCreator.ts

---
+++ src/github/pull-request/creator/ReleaseNotesSectionCreator.ts
@@ -0,0 +1,20 @@
+import { Release } from '../../releases'
+import type { GitHubUrlOptimizer } from './GitHubUrlOptimizer'
+
+// TODO: Add test
+export class ReleaseNotesSectionCreator {
+  constructor (private readonly gitHubUrlOptimizer: GitHubUrlOptimizer) {}
+
+  create (releases: Release[]): string {
+    const items = releases.map(({
+      tag,
+      url
+    }) => {
+      const optimizedUrl = this.gitHubUrlOptimizer.optimize(url).toString()
+      return `- [${tag}](${optimizedUrl})`
+    })
+    return `## Release notes
+
+${items.join('\n')}`
+  }
+}
