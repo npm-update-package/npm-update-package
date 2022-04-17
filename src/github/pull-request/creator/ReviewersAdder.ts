e8e9be556cd3b0ede6b7bc68b93c652de224f5ed
Munieru <20086673+munierujp@users.noreply.github.com>
2022-04-17 22:43:49 +0900

    Add ReviewersAdder.test.ts (#563)

--- src/github/pull-request/creator/ReviewersAdder.ts
+++ src/github/pull-request/creator/ReviewersAdder.ts
@@ -2,7 +2,6 @@ import shuffle from 'lodash/shuffle'
 import type { GitRepository } from '../../../git'
 import type { GitHub } from '../../GitHub'
 
-// TODO: Add test
 export class ReviewersAdder {
   private readonly github: GitHub
   private readonly gitRepo: GitRepository
