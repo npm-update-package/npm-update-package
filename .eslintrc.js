34ec9324e617c3b6cb4b0b06d1eb087fccc91e9f
Munieru <20086673+munierujp@users.noreply.github.com>
2022-04-19 23:11:15 +0900

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   extends: [
     'standard-with-typescript',
     'plugin:jest/recommended',
-    'plugin:lodash/recommended',
-    'plugin:you-dont-need-lodash-underscore/compatible'
+    'lodash'
   ],
   plugins: [
     'tsdoc'
@@ -22,10 +21,6 @@ module.exports = {
         caseInsensitive: true
       }
     }],
-    // Use native API rather than Lodash
-    'lodash/prefer-lodash-chain': 'off',
-    'lodash/prefer-lodash-method': 'off',
-    'lodash/prefer-lodash-typecheck': 'off',
     // Use logger
     'no-console': 'error'
   }
