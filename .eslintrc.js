01b733a3dcd4c4d3b998758e3d577d9590013118
Munieru <20086673+munierujp@users.noreply.github.com>
2022-05-18 00:51:43 +0900

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     'standard-with-typescript',
+    'plugin:unicorn/recommended',
     'plugin:eslint-comments/recommended',
     'plugin:jest/recommended',
     'lodash'
@@ -12,17 +13,28 @@ module.exports = {
     project: './tsconfig.json'
   },
   rules: {
-    // Better TSDoc
-    'tsdoc/syntax': 'warn',
-    // Sort imports alphabetically
-    'sort-imports': 'off',
     'import/order': ['error', {
       alphabetize: {
         order: 'asc',
         caseInsensitive: true
       }
     }],
-    // Use logger
-    'no-console': 'error'
+    'no-console': 'error',
+    'sort-imports': 'off',
+    'tsdoc/syntax': 'warn',
+    'unicorn/error-message': 'off',
+    'unicorn/filename-case': 'off',
+    // TODO: Maybe enable later
+    'unicorn/no-array-callback-reference': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/no-useless-undefined': 'off',
+    // TODO: Maybe enable later
+    'unicorn/prefer-module': 'off',
+    // TODO: Maybe enable later
+    'unicorn/prefer-spread': 'off',
+    // TODO: Maybe enable later
+    'unicorn/prefer-ternary': 'off',
+    'unicorn/prevent-abbreviations': 'off'
   }
 }
