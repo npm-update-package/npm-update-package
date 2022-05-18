ed4ad968a206606fcc01d0fc39a9493a618b9ad7
Munieru <20086673+munierujp@users.noreply.github.com>
2022-05-18 19:38:58 +0900

    Fix errors

--- src/json/toJSON.ts
+++ src/json/toJSON.ts
@@ -4,5 +4,5 @@ interface Options {
 
 export const toJSON = (value: unknown, options?: Options): string => {
   const pretty = options?.pretty ?? false
-  return pretty ? JSON.stringify(value, null, 2) : JSON.stringify(value)
+  return pretty ? JSON.stringify(value, undefined, 2) : JSON.stringify(value)
 }
