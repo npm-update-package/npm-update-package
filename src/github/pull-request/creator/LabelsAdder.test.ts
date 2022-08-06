99eb9eeef075489682239afd4041abd032d02813
Munieru <20086673+munierujp@users.noreply.github.com>
2022-08-06 17:20:51 +0900

    Add LabelsAdder.ts

---
+++ src/github/pull-request/creator/LabelsAdder.test.ts
@@ -0,0 +1,59 @@
+import type { GitRepository } from '../../../git'
+import type { Options } from '../../../options'
+import type { GitHub } from '../../GitHub'
+import { LabelsAdder } from './LabelsAdder'
+
+describe('LabelsAdder', () => {
+  describe('add', () => {
+    const addLabelsMock = jest.fn()
+    const github = {
+      addLabels: addLabelsMock
+    } as unknown as GitHub
+    const gitRepo = {
+      owner: 'npm-update-package',
+      name: 'example',
+      url: 'https://github.com/npm-update-package/example'
+    } as unknown as GitRepository
+    const issueNumber = 1
+
+    afterEach(() => {
+      jest.resetAllMocks()
+    })
+
+    it('adds `npm-update-package` and additional labels if additionalLabels option exists', async () => {
+      const options = {
+        additionalLabels: ['bot', 'dependencies']
+      } as unknown as Options
+      const labelsAdder = new LabelsAdder({
+        options,
+        github,
+        gitRepo
+      })
+      await labelsAdder.add(issueNumber)
+
+      expect(addLabelsMock).toBeCalledWith({
+        owner: gitRepo.owner,
+        repo: gitRepo.name,
+        issueNumber,
+        labels: ['npm-update-package', 'bot', 'dependencies']
+      })
+    })
+
+    it('adds only `npm-update-package` label if additionalLabels option does not exist', async () => {
+      const options = {} as unknown as Options
+      const labelsAdder = new LabelsAdder({
+        options,
+        github,
+        gitRepo
+      })
+      await labelsAdder.add(issueNumber)
+
+      expect(addLabelsMock).toBeCalledWith({
+        owner: gitRepo.owner,
+        repo: gitRepo.name,
+        issueNumber,
+        labels: ['npm-update-package']
+      })
+    })
+  })
+})
