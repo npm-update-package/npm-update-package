721f067d083b39a35d0bb3c07030d15f85663c39
Munieru <20086673+munierujp@users.noreply.github.com>
2022-09-22 22:52:50 +0900

    Use @jest/globals in src/github/pull-request/finder/**/*.test.ts

--- src/github/pull-request/finder/isPullRequestByNpmUpdatePackage.test.ts
+++ src/github/pull-request/finder/isPullRequestByNpmUpdatePackage.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from '@jest/globals'
 import type { PullRequest } from '../../GitHub'
 import { isPullRequestByNpmUpdatePackage } from './isPullRequestByNpmUpdatePackage'
 
