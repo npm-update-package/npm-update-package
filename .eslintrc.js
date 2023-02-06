03174c40b27fd70fd7834c4f4895ac2b5e8c68f2
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-06 20:27:40 +0900

    Enable sonarjs/no-small-switch

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     'sonarjs/no-nested-switch': 'error',
     'sonarjs/no-nested-template-literals': 'error',
     'sonarjs/no-redundant-boolean': 'error',
-    'sonarjs/no-redundant-jump': 'error'
+    'sonarjs/no-redundant-jump': 'error',
+    'sonarjs/no-small-switch': 'error'
   }
 }
