e4eecd520a3d969c7f52f4412456ca5c613b7b7b
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-06 20:35:25 +0900

    Enable sonarjs/no-collection-size-mischeck

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     'no-console': 'error',
     'sonarjs/max-switch-cases': 'error',
     'sonarjs/no-collapsible-if': 'error',
+    'sonarjs/no-collection-size-mischeck': 'error',
     'sonarjs/no-element-overwrite': 'error',
     'sonarjs/no-empty-collection': 'error',
     'sonarjs/no-extra-arguments': 'error',
