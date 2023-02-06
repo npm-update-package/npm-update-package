09cd714761abf9b5ac44b0a871d1e55a4bb09dd2
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-06 20:45:35 +0900

    Enable sonarjs/no-duplicated-branches

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'sonarjs/no-all-duplicated-branches': 'error',
     'sonarjs/no-collapsible-if': 'error',
     'sonarjs/no-collection-size-mischeck': 'error',
+    'sonarjs/no-duplicated-branches': 'error',
     'sonarjs/no-element-overwrite': 'error',
     'sonarjs/no-empty-collection': 'error',
     'sonarjs/no-extra-arguments': 'error',
