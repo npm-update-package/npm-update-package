6f7b872c008b035aa7b295ca9bdf5b75d5aecaa8
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-08 02:08:41 +0900

    Add eslint-plugin-sonarjs (#1151)
    * Add eslint-plugin-sonarjs
    
    * Add sonarjs to plugins
    
    * Add lint:fix
    
    * Enable sonarjs/max-switch-cases
    
    * Enable sonarjs/no-collapsible-if
    
    * Enable sonarjs/no-element-overwrite
    
    * Enable sonarjs/no-empty-collection
    
    * Enable sonarjs/no-extra-arguments
    
    * Enable sonarjs/no-ignored-return
    
    * Enable sonarjs/no-nested-switch
    
    * Enable sonarjs/no-nested-template-literals
    
    * Enable sonarjs/no-redundant-boolean
    
    * Enable sonarjs/no-redundant-jump
    
    * Enable sonarjs/no-small-switch
    
    * Enable sonarjs/no-unused-collection
    
    * Enable sonarjs/no-use-of-empty-return-value
    
    * Enable sonarjs/no-useless-catch
    
    * Enable sonarjs/non-existent-operator
    
    * Enable sonarjs/prefer-immediate-return
    
    * Enable sonarjs/prefer-object-literal
    
    * Enable sonarjs/prefer-single-boolean-return
    
    * Enable sonarjs/prefer-while
    
    * Enable sonarjs/no-collection-size-mischeck
    
    * Enable sonarjs/no-gratuitous-expressions
    
    * Enable sonarjs/no-identical-conditions
    
    * Enable sonarjs/no-identical-expressions
    
    * Enable sonarjs/no-identical-functions
    
    * Enable sonarjs/no-same-line-conditional
    
    * Enable sonarjs/no-all-duplicated-branches
    
    * Enable sonarjs/no-duplicated-branches
    
    * Enable sonarjs/no-one-iteration-loop
    
    * Beautify
    
    * Enable sonarjs/cognitive-complexity
    
    * Refactoring
    
    * Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   extends: [
     '@npm-update-package/eslint-config-typescript',
+    'plugin:sonarjs/recommended',
     'plugin:jest/recommended'
   ],
   parserOptions: {
     project: './tsconfig.json'
   },
   rules: {
-    'no-console': 'error'
+    'no-console': 'error',
+    'sonarjs/no-duplicate-string': 'off'
   }
 }
