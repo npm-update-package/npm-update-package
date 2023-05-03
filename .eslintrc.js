be7b11586c5311e2d0d90d1e6c3646a951ae1c94
Munieru <20086673+munierujp@users.noreply.github.com>
2023-05-03 17:58:49 +0900

    Remove extra deps (#1298)
    * Remove extra deps
    
    * Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,12 @@
 module.exports = {
   extends: [
     '@npm-update-package/eslint-config-typescript',
-    'plugin:sonarjs/recommended',
     'plugin:jest/recommended'
   ],
   parserOptions: {
     project: './tsconfig.json'
   },
   rules: {
-    'no-console': 'error',
-    'sonarjs/no-duplicate-string': 'off'
+    'no-console': 'error'
   }
 }
