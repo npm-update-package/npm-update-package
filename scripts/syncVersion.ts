fab0ffe9954618d0951af371c88538dbdca65553
Munieru <20086673+munierujp@users.noreply.github.com>
2024-05-06 22:29:40 +0900

    Refactoring

--- scripts/syncVersion.ts
+++ scripts/syncVersion.ts
@@ -4,7 +4,6 @@ import {
 } from 'node:fs/promises'
 
 const packageJSON = JSON.parse(await readFile('./package.json', 'utf8'))
-const currentVersion = packageJSON.version
 const appTS = await readFile('./src/app.ts', 'utf8')
-const newAppTS = appTS.replace(/export const version = '.+'/, `export const version = '${currentVersion}'`)
+const newAppTS = appTS.replace(/export const version = '.+'/, `export const version = '${packageJSON.version}'`)
 await writeFile('./src/app.ts', newAppTS)
