ec399e0ee5a5af51a09c4edf8b5baa2c15c0fb97
Munieru <20086673+munierujp@users.noreply.github.com>
2024-05-27 01:46:41 +0900

    Refactoring

--- src/github/pull-request/creator/GitHubUrlOptimizer.test.ts
+++ src/github/pull-request/creator/GitHubUrlOptimizer.test.ts
@@ -15,19 +15,19 @@ await describe('GitHubUrlOptimizer', async () => {
         prBodyGithubHost: 'github.test'
       } as unknown as Options
       const gitHubUrlOptimizer = new GitHubUrlOptimizer(options)
-      const inputs: Array<[url: string | URL, expected: URL]> = [
-        ['https://github.com/foo/', new URL('https://github.test/foo/')],
-        [new URL('https://github.com/foo/'), new URL('https://github.test/foo/')],
-        ['https://foo.github.com/bar/', new URL('https://foo.github.com/bar/')],
-        [new URL('https://foo.github.com/bar/'), new URL('https://foo.github.com/bar/')],
-        ['https://github.com.test/foo/', new URL('https://github.com.test/foo/')],
-        [new URL('https://github.com.test/foo/'), new URL('https://github.com.test/foo/')]
+      const inputs: Array<[url: string | URL, expected: string]> = [
+        ['https://github.com/foo/', 'https://github.test/foo/'],
+        [new URL('https://github.com/foo/'), 'https://github.test/foo/'],
+        ['https://foo.github.com/bar/', 'https://foo.github.com/bar/'],
+        [new URL('https://foo.github.com/bar/'), 'https://foo.github.com/bar/'],
+        ['https://github.com.test/foo/', 'https://github.com.test/foo/'],
+        [new URL('https://github.com.test/foo/'), 'https://github.com.test/foo/']
       ]
       each(inputs, ({ title }, [url, expected]) => {
         void it(title, () => {
           const actual = gitHubUrlOptimizer.optimize(url)
 
-          assert.strictEqual(actual.toString(), expected.toString())
+          assert.strictEqual(actual.toString(), expected)
         })
       })
     })
