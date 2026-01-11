1f3d286a141d017fd2ce39347806ab686b517d17
Munieru <20086673+munierujp@users.noreply.github.com>
2026-01-11 13:04:44 +0900

    chore: remove unnecessary blank lines in PackageManagerName and SemVerLevel files

--- src/package-manager/PackageManagerName.ts
+++ src/package-manager/PackageManagerName.ts
@@ -5,5 +5,4 @@ export const PackageManagerName = {
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type PackageManagerName = typeof PackageManagerName[keyof typeof PackageManagerName]
 const packageManagerNames = Object.values(PackageManagerName)
-
 export const isPackageManagerName = (value: any): value is PackageManagerName => packageManagerNames.includes(value)
