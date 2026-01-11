b22b9a9fd9c03ee8b5d8086c2e4a9f4f28a527c9
Munieru <20086673+munierujp@users.noreply.github.com>
2026-01-11 13:01:17 +0900

    chore: update eslint configuration to include global ignores for dist directory

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 // @ts-check
 
+import { globalIgnores } from 'eslint/config'
 import neostandard from 'neostandard'
 import comments from '@eslint-community/eslint-plugin-eslint-comments/configs'
 
 export default [
+  globalIgnores(['dist/']),
   ...neostandard({
     ts: true
   }),
